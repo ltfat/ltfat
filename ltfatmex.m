1118adb58dec3be1d226fc22c3f641bcab7d9b72
Peter L. Soendergaard <soender@users.sourceforge.net>
2010-05-17 15:52:05 +0200

    added test possibility to ltfatmex

--- ltfatmex.m
+++ ltfatmex.m
@@ -23,82 +23,139 @@ if numel(TF_CONF)==0
     disp('');
     return;
 end;
-
+  
 bp=TF_CONF.basepath;
 
-defnopos.flags.target={'auto','lib','mex','oct','gpc'};
-defnopos.flags.command={'compile','clean'};
+return;
+
+defnopos.flags.target={'auto','lib','mex','gpc'};
+defnopos.flags.command={'compile','clean','test'};
 [flags,kv]=ltfatarghelper({},defnopos,varargin);
 
 % Remember the current directory.
 curdir=pwd;
 
-do_lib  = flags.do_lib || (flags.do_auto && isoctave);
-do_oct  = flags.do_oct || (flags.do_auto && isoctave);
-do_mex  = flags.do_mex || (flags.do_auto && ~isoctave);
+do_lib  = flags.do_lib || (flags.do_auto && (isoctave || isunix));
+do_mex  = flags.do_mex || flags.do_auto;
 do_gpc  = flags.do_gpc || (flags.do_auto && ~isoctave);
 
+if isoctave
+  extname='oct';
+  ext='oct';
+else
+  extname='mex';
+  ext=mexext;
+end;
+
+% -------------- Handle cleaning --------------------------------
+  
 if flags.do_clean
     
-    if ~isoctave
-        % Delete files permanently (bypass trashcan on Windows/Mac
-        % but remember the old state
-        oldstate = recycle('off');
-    end;
-        
-    if do_oct
-        deletefiles([bp,'oct'],'*.oct');
-        deletefiles([bp,'oct'],'*.o');
-    end;
-    
-    if do_mex
-        deletefiles([bp,'mex'],['*.',mexext]);
-    end;
-    
-    if do_gpc
-        deletefiles([bp,'thirdparty',filesep,'PolygonClip'],
-        ['PolygonClip.',mexext]);
-    end;
-    
-    if ~isoctave
-        recycle(oldstate);
-    end;
-    
-    return;
-    
-end;
-
-if do_lib
-    s=['cd ',bp,'src; make'];
-    s
-    system(s);    
-end;
-
-if do_oct
-    if compile_ltfat(bp)>1;
-        
-        if isoctave
-            extname='oct';
-        else
-            extname='mex';
-        end;
-        
-        fprintf('The %s interfaces was not built.', extname);
-    end;
-end;
-
-    
-
-if do_gpc &&  ~isoctave
-    if exist([bp,filesep,'thirdparty',filesep,'GPC',filesep,'gpc.c'], ...
-             'file')
-        % Compile the PolygonClip interface to GPC for use with mulaclab
-        cd([bp,filesep,'thirdparty',filesep,'PolygonClip']);
-        mex('-I../GPC','PolygonClip.c','../GPC/gpc.c');
+  if ~isoctave
+    % Delete files permanently (bypass trashcan on Windows/Mac
+    % but remember the old state
+    oldstate = recycle('off');
+  end;
+  
+  if do_lib
+    disp('========= Cleaning libltfat ===============');
+    s=['cd ',bp,'src; make clean'];
+    system(s);
+    disp('Done.');    
+  end;
+  
+  if do_mex
+    fprintf('========= Cleaning %s interfaces ==========\n', extname);
+    if isoctave
+      deletefiles([bp,'oct'],'*.oct');
+      deletefiles([bp,'oct'],'*.o');
     else
-        error('Unable to compile GPC');
-        
+      deletefiles([bp,'mex'],['*.',mexext]);
     end;
+  end;
+  
+  if do_gpc
+    disp('========== Cleaning GPC ==============');
+    deletefiles([bp,'thirdparty',filesep,'PolygonClip'],['PolygonClip.',mexext]);
+  end;
+  
+  if ~isoctave
+    recycle(oldstate);
+  end;  
+  
+end;
+
+% -------------- Handle compiling  --------------------------------
+
+if flags.do_compile
+  if do_lib
+    disp('========== Compiling libltfat ==============');
+    s=['cd ',bp,'src; '];
+    if isoctave
+      [status,output]=system([s,'mkoctfile -p CC']);
+      system([s,'make CC=',output]);
+    else
+      system([s,'make']);
+    end;
+    disp('Done.');
+  end;
+  
+  if do_mex
+    fprintf('========= Compiling %s interfaces ==========\n', extname);
+    if compile_ltfat(bp)>1;                
+      fprintf('ERROR: The %s interfaces was not built.\n', extname);
+    else
+      disp('Done.');
+    end;
+  end;
+  
+  if do_gpc
+    disp('========= Compiling GPC ===============');
+    if exist([bp,'thirdparty',filesep,'GPC',filesep,'gpc.c'], ...
+             'file')
+      % Compile the PolygonClip interface to GPC for use with mulaclab
+      cd([bp,'thirdparty',filesep,'PolygonClip']);
+      mex('-I../GPC','PolygonClip.c','../GPC/gpc.c');
+      disp('Done.');
+    else
+      disp('ERROR: Unable to compile GPC');
+    end;
+  end;
+end;
+
+% -------------- Handle testing ---------------------------------------
+
+if flags.do_test
+  
+  if do_mex
+    
+    fprintf('========= Testing %s interfaces ==========\n', extname);
+    fprintf('1.: Test if comp_pgauss.%s was compiled: ',extname);
+    fname=[bp,extname,filesep,'comp_pgauss.',ext];
+    if exist(fname,'file')
+      disp('SUCCESS.');
+    else
+      disp('FAILED.');
+    end;
+    
+    fprintf('2.: Test if pgauss executes:              ');
+    pgauss(100);
+    % If the execution of the script makes it here, we now that pgauss
+    % did not crash the system, so we can just print success. Same story
+    % with the following entries.
+    disp('SUCCESS.');
+
+    fprintf('3.: Test if fftreal executes:             ');
+    fftreal(randn(10,1),10);
+    disp('SUCCESS.');
+
+    fprintf('4.: Test if dgt executes:                 ');
+    dgt(randn(12,1),randn(12,1),3,4);
+    disp('SUCCESS.');
+
+    
+  end;
+  
 end;
 
 % Jump back to the original directory.
@@ -122,7 +179,7 @@ status=1;
 % Determine the name of the ltfat library
 s=[bp,'lib',filesep,'libltfat-nomem.a'];
 
-if ispc || ~isoctave    
+if ispc && ~isoctave    
     s=[bp,'lib',filesep,'libltfat-nomem.lib'];
 end;
 
