af7c68249a787ab0e9684ef10ab9a161dfab55c9
Peter L. Soendergaard <soender@users.sourceforge.net>
2010-07-02 14:28:38 +0200

    added more test cases to the nsdgt tester

--- testing/test_nsdgt.m
+++ testing/test_nsdgt.m
@@ -17,43 +17,54 @@ test_failed=0;
 
 disp(' ===============  TEST_NSDGT ================');
   
-% Parameters
-sigLen = 450;
-nbTimePosition = 11;
-winType = 'hann'; % type of window
-width = round(linspace(32,256,nbTimePosition)); % width of the windows
-M=width;
-a=round(width/4);
+for ii=1:3
+  % Parameters
+  sigLen = 450;
+  nbTimePosition = 11;
+  winType = 'hann'; % type of window
+  width = round(linspace(32,256,nbTimePosition)); % width of the windows
+  M=width;
+  if ii==2
+    % Make M longer than the window length.
+    M=M+2;
+  end;
+  
+  if ii==3
+    % Make M shorter than the window length.
+    M=M-2;
+  end;
+  
+  
+  a=round(width/4);
+    
+  win = cell(nbTimePosition, 1);
+  for k=1:nbTimePosition
+    win{k}=firwin(winType,width(k));
+  end
 
-%timePosition = round(cumsum(width/4)); % position in time of the windows
-timePosition = cumsum(a); % position in time of the windows
-timePosition = timePosition - timePosition(1);
-
-win = cell(nbTimePosition, 1);
-for k=1:nbTimePosition
-  win{k}=firwin(winType,width(k));
-end
 
 
+  sig = randn(sigLen,1)-0.5; % random signal
+  
+  for jj=1:2
+    if jj==1
+      winn = win;
+      wind = nsgabdual(win,a,sigLen);
+      wintype='DUAL ';
+    else    
+      wind = nsgabtight(win,a,sigLen);
+      winn = wind;
+      wintype='TIGHT';
+    end;
+    
+    [c,Ls] = nsdgt(sig,winn,a,M);
+    sigd=insdgt(c,wind,a,sigLen);
+    
+    res=norm(sigd-sig)/ norm(sig);
+    [test_failed,fail]=ltfatdiditfail(res,test_failed);
+    fprintf(['%s %0.5g %s\n'],wintype,res,fail);
+    
+  end;
+  
+end;
 
-sig = rand(sigLen,1)-0.5; % random signal
-
-% test using dual windows
-wind = nsgabdual(win,a,sigLen);
-[c,Ls] = nsdgt(sig,win,a,width);
-sigd=insdgt(c,wind,a,sigLen);
-
-res=norm(sigd-sig)/ norm(sig);
-[test_failed,fail]=ltfatdiditfail(res,test_failed);
-fprintf(['DUAL  %0.5g %s\n'],res,fail);
-
-% test using tight windows
-wint = nsgabtight(win,a,sigLen);
-[ct,Ls] = nsdgt(sig,wint,a,width);
-sigt=insdgt(ct,wint,a,sigLen);
-
-res=norm(sigt-sig)/ norm(sig);
-[test_failed,fail]=ltfatdiditfail(res,test_failed);
-fprintf(['TIGHT %0.5g %s\n'],res,fail);
-
-[A,B]=nsgabframebounds(win,a,sigLen);
