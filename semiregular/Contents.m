b367260db9dd4851a4fba53c1242285b429050b0
Peter L. Soendergaard <soender@users.sourceforge.net>
2010-12-16 12:01:15 +0100

    added real* to Contents

--- semiregular/Contents.m
+++ semiregular/Contents.m
@@ -3,11 +3,14 @@
 %  Florent Jaillet and Peter L. Soendergaard, 2010
 %
 %  Filter banks
-%    FILTERBANK       - Filter bank
-%    IFILTERBANK      - Inverse filter bank
-%    FILTERBANKDUAL   - Canonical dual filters
-%    FILTERBANKTIGHT  - Canonical tight filters
-%    FILTERBANKBOUNDS - Frame bounds of filter bank
+%    FILTERBANK           - Filter bank
+%    IFILTERBANK          - Inverse filter bank
+%    FILTERBANKDUAL       - Canonical dual filters
+%    FILTERBANKTIGHT      - Canonical tight filters
+%    FILTERBANKREALDUAL   - Canonical dual filters for real signals
+%    FILTERBANKREALTIGHT  - Canonical tight filters for real signals
+%    FILTERBANKBOUNDS     - Frame bounds of filter bank
+%    FILTERBANKREALBOUNDS - Frame bounds of filter bank for real signals
 %
 %  Gabor systems with time varying windows
 %    NSDGT            - Non-stationary DGT
