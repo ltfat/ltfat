eeca7b768b0630f15881e1a91bddd7ffa5ce4edb
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-04-28 11:41:31 +0200

    re-added filterbank routines, now with error when used in the unimplemented case.

---
+++ semiregular/filterbankrealbounds.m
@@ -0,0 +1,79 @@
+function [AF,BF]=filterbankrealbounds(g,a,varargin);
+%FILTERBANKBOUNDS  Frame bounds of filter bank for real signals only
+%   Usage: fcond=filterbankbounds(g,a);
+%          [A,B]=filterbankbounds(g,a);
+%
+%   FILTERBANKREALBOUNDS(g,a) calculates the ratio B/A of the frame bounds of
+%   the filterbank specified by g and _a. The ratio is a measure of the
+%   stability of the system.  Use this function on the common construction
+%   where the filters in g only covers the positive frequencies.
+%
+%   [A,B]=FILTERBANKREALBOUNDS(g,a) returns the lower and upper frame bounds
+%   explicitly.
+%
+%   See also: filterbank, filterbankdual
+  
+if nargin<2
+  error('%s: Too few input parameters.',upper(mfilename));
+end;
+
+[a,M,longestfilter,lcm_a]=assert_filterbankinput(g,a);
+
+
+definput.keyvals.L=[];
+[flags,kv,L]=ltfatarghelper({'L'},definput,varargin);
+
+if isempty(L)
+  L=ceil(longestfilter/lcm_a)*lcm_a;
+end;
+
+AF=Inf;
+BF=0;
+  
+if all(a==a(1))
+  % Uniform filterbank, use polyphase representation
+  a=a(1);
+  
+  N=L/a;
+
+  G=zeros(L,M);
+  for ii=1:M
+    G(:,ii)=fft(fir2long(g{ii},L));
+  end;
+  
+  Ha=zeros(a,M);
+  Hb=zeros(a,M);
+  
+  for w=0:N-1
+    idx_a = mod(w-(0:a-1)*N,L)+1;
+    idx_b = mod((0:a-1)*N-w,L)+1;
+    Ha = G(idx_a,:);
+    Hb = conj(G(idx_b,:));
+    
+    % A 'real' is needed here, because the matrices are known to be
+    % Hermitian, but sometimes Matlab/Octave does not recognize this.  
+    work=real(eig(real(Ha*Ha'+Hb*Hb')));
+    
+    AF=min(AF,min(work));
+    BF=max(BF,max(work));
+    
+  end;
+  
+  AF=AF/a;
+  BF=BF/a;
+  
+else
+
+    error('Not implemented yet.');
+  
+end;
+
+if nargout<2
+  % Avoid the potential warning about division by zero.
+  if AF==0
+    AF=Inf;
+  else
+    AF=BF/AF;
+  end;
+end;
+  
---
+++ semiregular/filterbankrealtight.m
@@ -0,0 +1,73 @@
+function gtout=filterbankrealtight(g,a,varargin);
+%FILTERBANKTIGHT  Tight filters of filterbank for real signals only 
+%   Usage:  gd=filterbanktight(g,a);
+%
+%   FILTERABANKTIGHT(g,a) computes the canonical tight filters of g for a
+%   channel subsampling rate of _a (hop-size). The tight filters work only
+%   for real-valued signals. Use this function on the common construction
+%   where the filters in g only covers the positive frequencies.
+%
+%   The format of the filters g are described in the
+%   help of FILTERANK.
+%
+%   To actually invert the output of a filterbank, use the tight filters
+%   together with 2*real(IFILTERBANK(...)) function.
+%
+%   See also: filterbank, ifilterbank
+
+if nargin<2
+  error('%s: Too few input parameters.',upper(mfilename));
+end;
+
+[a,M,longestfilter,lcm_a]=assert_filterbankinput(g,a);
+
+definput.keyvals.L=[];
+[flags,kv,L]=ltfatarghelper({'L'},definput,varargin);
+
+if isempty(L)
+  L=ceil(longestfilter/lcm_a)*lcm_a;
+end;
+
+if all(a==a(1))
+  % Uniform filterbank, use polyphase representation
+  a=a(1);
+  
+  G=zeros(L,M);
+  for ii=1:M
+    G(:,ii)=fft(fir2long(g{ii},L));
+  end;
+  
+  N=L/a;
+
+  gt=zeros(N,M);
+  
+  for w=0:N-1
+    idx_a = mod(w-(0:a-1)*N,L)+1;
+    idx_b = mod((0:a-1)*N-w,L)+1;
+    Ha = G(idx_a,:);
+    Hb = conj(G(idx_b,:));
+    
+    Ha=sqrtm(Ha*Ha'+Hb*Hb')\Ha;
+    
+    gt(idx_a,:)=Ha;
+  end;
+  
+  gt=ifft(gt)*sqrt(a);
+  
+  if isreal(g)
+    gt=real(gt);
+  end;
+  
+  gtout=cell(1,M);
+  for m=1:M
+    gtout{m}=gt(:,m);
+  end;
+  
+else
+
+  error('Not implemented yet.');
+  
+end;
+
+
+
