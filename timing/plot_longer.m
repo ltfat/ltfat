053824e1b8b06aeecd367c68dff3a1ed2d8ff7c8
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-04-29 14:50:57 +0200

    use nextfastfft in plot_longer

--- timing/plot_longer.m
+++ timing/plot_longer.m
@@ -3,12 +3,22 @@ data_fac=load("longer_fac.log");
 
 % Columns in data, fb : a M L W gl time
 % Columns in data, fac: a M L W time
-Ls=data_fb(:,3);
+Ls=data_fac(:,3);
 t_fb =data_fb(:,6);
 t_fac=data_fac(:,5);
 figure(1);
 
-plot(Ls,t_fb,Ls,t_fac);
-legend('FB','FAC');
-xlabel('Signal length / samples');
-ylabel('Running time / seconds');
+M=data_fac(1,2);
+L=data_fac(:,3);
+NL=nextfastfft(L/M);
+Nt_fac=t_fac(NL-9);
+
+if 0
+  plot(Ls,t_fb,Ls,t_fac);
+  legend('FB','FAC');
+  xlabel('Signal length / samples');
+  ylabel('Running time / seconds');
+else
+  plot(Ls,t_fb,Ls,Nt_fac);
+end;
+
