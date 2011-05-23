dbb197393b18037c3195a470556bcb92653dcbee
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-05-23 15:06:12 +0200

    added ref_dgt_pconv

---
+++ reference/ref_dgt_pconv.m
@@ -0,0 +1,17 @@
+function c=ref_dgt_pconv(f,g,a,M);
+%REF_DGT_PCONV  Compute a DGT using PCONV
+
+  
+L=size(f,1);
+
+N=L/a;
+b=L/M;
+
+c=zeros(M,N);
+
+for m=0:M-1
+  work = pconv(f,involute(g).*expwave(L,m*b));
+  c(m+1,:) = reshape(work(1:a:L),1,N);
+end;
+
+c=phaseunlock(c,a);
