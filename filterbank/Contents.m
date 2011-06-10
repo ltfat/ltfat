e49e83d5bb74bf1a73a4740d59dd663592bf039a
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-06-10 15:18:21 +0200

    more missing files

---
+++ filterbank/Contents.m
@@ -0,0 +1,22 @@
+% LTFAT - Filterbanks
+%
+%  Peter L. Soendergaard, 2011
+%
+%  Transforms
+%    UFILTERBANK          - Uniform Filter bank
+%    IUFILTERBANK         - Inverse Uniform filter bank
+%    FILTERBANK           - Filter bank
+%
+%  Window construction and bounds
+%    FILTERBANKDUAL       - Canonical dual filters
+%    FILTERBANKTIGHT      - Canonical tight filters
+%    FILTERBANKREALDUAL   - Canonical dual filters for real signals
+%    FILTERBANKREALTIGHT  - Canonical tight filters for real signals
+%    FILTERBANKBOUNDS     - Frame bounds of filter bank
+%    FILTERBANKREALBOUNDS - Frame bounds of filter bank for real signals
+%
+%  Plots
+%    PLOTFILTERBANK       - Plot filterbank and ufilterbank coefficients
+%
+%  For help, bug reports, suggestions etc. please send email to
+%  ltfat-help@lists.sourceforge.net
