316b0262110cb1e0a1dddf36125273d112923905
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-06-24 11:41:23 +0200

    added optional tolerance to ltfatdiditfail

--- testing/ltfatdiditfail.m
+++ testing/ltfatdiditfail.m
@@ -1,12 +1,16 @@
-function [test_failed,fail]=ltfatdiditfail(res,test_failed);
+function [test_failed,fail]=ltfatdiditfail(res,test_failed,tolerance);
 %LTFATDIDITFAIL  Did a test fail
 %
 %  [test_fail,fail]=LTFATDIDITFAIL(res,test_fail) updates test_fail if
 %  res is above threshhold and outputs the word FAIL in the variable
 %  fail. Use only in testing scripts.
   
+if nargin<3
+  tolerance=1e-10;
+end;
+  
 fail='';
-if (abs(res)>10e-10) || isnan(res)
+if (abs(res)>tolerance) || isnan(res)
   fail='FAILED';
   test_failed=test_failed+1;
 end;
