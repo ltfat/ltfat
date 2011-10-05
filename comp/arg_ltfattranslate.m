eabae17f6838113dd6ee26d540da97f59db62ee7
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-10-05 10:58:56 +0200

    forgot to add ltfattranslate

---
+++ comp/arg_ltfattranslate.m
@@ -0,0 +1,7 @@
+function definput=arg_tfplot(definput)
+
+  definput.keyvals.frequency='Frequency';
+  definput.keyvals.time='Time';
+  definput.keyvals.samples='samples';
+  definput.keyvals.normalized='normalized';
+  definput.keyvals.magnitude='Magnitude';
