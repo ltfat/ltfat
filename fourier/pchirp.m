baf85abe7d975c96a3113a12ca9758a99d747072
Peter L. Soendergaard <soender@users.sourceforge.net>
2012-08-13 12:06:18 +0200

    fixed pchirp for odd lengths

--- fourier/pchirp.m
+++ fourier/pchirp.m
@@ -4,7 +4,7 @@ function g=pchirp(L,n)
 %
 %   `pchirp(L,n)` returns a periodic, discrete chirp of length *L* that
 %   revolves *n* times around the time-frequency plane in frequency. *n* must be
-%   a whole number.
+%   an integer number.
 %
 %   To get a chirp that revolves around the time-frequency plane in time,
 %   use ::
@@ -13,16 +13,16 @@ function g=pchirp(L,n)
 %
 %   The chirp is computed by:
 %   
-%   ..  g(l+1) = exp(pi*i*n*l^2/L) for l=0,...,L-1
+%   ..  g(l+1) = exp(pi*i*n*l^2*(L+1)/L) for l=0,...,L-1
 %
-%   .. math:: g\left(l+1\right)=e^{\pi inl^{2}/L},\quad l=0,\ldots,L-1
+%   .. math:: g\left(l+1\right)=e^{\pi inl^{2}(L+1)/L},\quad l=0,\ldots,L-1
 %
 %   The chirp has absolute value 1 everywhere. To get a chirp with unit
 %   $l^2$-norm, divide the chirp by $\sqrt(L)$.
 %
 %   See also: dft, expwave
 %
-%   References: fehakamane06
+%   References: feichtinger2008metaplectic
 
 %   AUTHOR : Peter Soendergaard
 %   TESTING: OK
@@ -31,5 +31,11 @@ function g=pchirp(L,n)
 error(nargchk(2,2,nargin));
 
 % Compute normalized chirp
-g=(exp((0:L-1).^2/L*pi*i*n)/sqrt(L)).';
 
+% Old code, has low numerical precision and do not work correctly for odd legths.
+%g=(exp((0:L-1).^2/L*pi*i*n)/sqrt(L)).';
+
+% Compute normalized chirp
+m = 0:L-1;
+X = mod(n*m.^2*(L+1),2*L);
+g = (exp(pi*1i*X/L)/sqrt(L)).';
