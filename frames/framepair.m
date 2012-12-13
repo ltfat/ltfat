31deff7c852831bf909e4bbf57dd43a09763589a
Peter L. Søndergaard <soender@users.sourceforge.net>
2012-12-13 17:09:35 +0100

    forgot to add all the new frame files

---
+++ frames/framepair.m
@@ -0,0 +1,58 @@
+function [F1,F2]=framepair(ftype,g1,g2,varargin);
+%FRAMEPAIR  Construct a new frame
+%   Usage: F=framepair(ftype,g1,g2,...);
+%
+%   `[F1,F2]=framepair(ftype,g1,g2,...)` constructs two new frame objects *F* of
+%   the same type *ftype* using the windows *g1* and *g2*. The windows
+%   are specific to choosen frame type. See the help on *frame* for the
+%   windows and arguments. 
+%
+%   This function makes it easy to create a pair of canonical dual frames:
+%   simply specify `'dual'` as window if one frame should be the dual of the
+%   other.
+%
+%   This is most easily explained through some examples. The following
+%   example creates a Gabor frame for real-valued signals with a Gaussian
+%   analysis window and its canonical dual frame as the synthesis frame:::
+%
+%      f=greasy;
+%      [Fa,Fs]=framepair('dgtreal','gauss','dual',20,294);
+%      c=frana(Fa,f);
+%      r=frsyn(Fs,c);
+%      norm(f-r)
+%
+%   The following example creates a Wilson basis with a Gaussian
+%   synthesis window, and its canonical dual frame as the analysis
+%   frame::
+% 
+%     [Fa,Fs]=framepair('dwilt','dual','gauss',20);
+%
+%   See also: frame, framedual, frametight
+
+  
+if nargin<1
+  error('%s: Too few input parameters.',upper(mfilename));
+end;
+
+if ~ischar(ftype)
+  error(['%s: First agument must be a string denoting the type of ' ...
+         'frame.'],upper(mfilename));
+end;
+
+ftype=lower(ftype);
+
+if ~strcmp(g1,'dual')
+    F1=frame(ftype,g1,varargin{:});
+end;
+
+if ~strcmp(g2,'dual')
+    F2=frame(ftype,g2,varargin{:});
+end;
+
+if strcmp(g1,'dual')
+    F1=framedual(F2);
+end;
+
+if strcmp(g2,'dual')
+    F2=framedual(F1);
+end;
