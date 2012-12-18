5873867b78eb04f161af924e24d3e5238613903b
Peter L. Søndergaard <soender@users.sourceforge.net>
2012-12-18 21:25:45 +0100

    added cutaway file

---
+++ wavelets/Contents_cutaway.m
@@ -0,0 +1,14 @@
+%      WFILT_LEMAIRE         - Battle and Lemarie quadrature filters
+%      WFILT_MATLABWTWRAPPER - Wrapper of the wfilters function from the Matlab Wavelet Toolbox 
+%      WFILT_MAXFLAT         - Maximally flat FIR filters
+%      WFILT_MBAND           - M-band filters
+%      WFILT_OPTFS           - Optimized orthogonal filters with improved Frequency Selectivity (ortonormal base)
+%      WFILT_REMEZ           - Wavelet orthonogal filters based on the Remez Exchange algorithm
+%      WFILT_SYMDS           - SYMmetric wavelet Dyadic Siblings (frames)
+%      WFILT_SPLINE          - Biorthogonal spline wavelet filters
+%      WFILT_SYM             - Least asymmetric Daubechies wavelet filters
+%      
+%   Wavelet filters defined in the frequency-domain
+%      WFREQ_RADWT           - Overcomplete Rational-Dilation wavelet transform
+%      WFREQ_LEMAIRE         - Battle and Lemarie wavelet filters
+%
