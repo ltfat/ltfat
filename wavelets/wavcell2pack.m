5655f89fcc6e6005923e89e26698bb30f436ac77
prusa <prusa@users.sourceforge.net>
2013-02-25 17:37:10 +0100

    wavcell2pack.m and wavpack2cell.m now compatible with the new format of fwt including dim.

--- wavelets/wavcell2pack.m
+++ wavelets/wavcell2pack.m
@@ -1,67 +1,56 @@
 function [cvec,Lc] = wavcell2pack(ccell,varargin)
 %WAVCELL2PACK Changes wavelet coefficients storing format
 %   Usage:  [cvec,Lc] = wavcell2pack(ccell);
+%           [cvec,Lc] = wavcell2pack(ccell,dim);
 %
 %   Input parameters:
 %         ccell    : Coefficients stored in a collumn cell-array.
+%         dim      : Identifies dimension along which the data were transformed. 
 %
 %   Output parameters:
 %         cvec     : Coefficients in packed format.
 %         Lc       : Vector containing coefficients lengths.
 %
-%   *cvec* is column vector or matrix with *W* columns for multi-channel inputs containing
-%   coefficients in the packed format. Coefficients are stored as follows:
-%   `cvec(1:Lc(1),w)` - approximation coefficients at level *J* of the channel *w*,
-%   `cvec(1+sum(Lc(1:j-1)):sum(Lc(1:j),w)` for *j>1*.
+%   This function transformes coefficients stored as elements of cell-array *ccell*
+%   collumns/matrices to a single vector/matrix.
+%   *cvec* is vector or matrix containing coefficients in the packed format.
+%   For dim==1, the coefficients are stored as collumns:
+%   cvec(1:Lc(1),w) - approximation coefficients at level *J* of the channel *w*,
+%   cvec(1+sum(Lc(1:j-1)):sum(Lc(1:j),w) for *j>1*. For dim==2 as rows in
+%   the similar manner.
 %
 
 if(nargin<1)
     error('%s: Too few input parameters.',upper(mfilename));
 end
 
-definput.flags.format = {'channels','onecol'};
-[flags,kv]=ltfatarghelper({},definput,varargin);
-
-JJtotal = length(ccell);
-
-
-if(flags.do_channels)
-   [cLen, W] = size(ccell{end});
-
-   Lc = zeros(JJtotal,1);
-   for jj=1:JJtotal
-      Lc(jj) =  size(ccell{jj},1);
-   end
-
-   cvec = zeros(sum(Lc),W);
-
-   lenSumIdx = 1;
-   lenSum = 0;
-   for jj=1:JJtotal
-      cvec(1+lenSum:Lc(lenSumIdx)+lenSum,:) = ccell{jj};
-      lenSum = lenSum+Lc(lenSumIdx);
-      lenSumIdx=lenSumIdx+1;
-   end
-elseif(flags.do_onecol)
-   cLens = zeros(JJtotal,1); 
-   Lc = cell(JJtotal,1);
-   for jj=1:JJtotal
-      Lc{jj} =  size(ccell{jj});
-      cLens(jj) = prod(Lc{jj});
-   end
-   
-   cvec = zeros(sum(cLens),1);
-   
-   lenSumIdx = 1;
-   lenSum = 0;
-   for jj=1:JJtotal
-      cvec(1+lenSum:cLens(lenSumIdx)+lenSum,:) = ccell{jj}(:);
-      lenSum = lenSum+cLens(lenSumIdx);
-      lenSumIdx=lenSumIdx+1;
-   end 
-    
-else
-    error('Should not get here.');
+definput.keyvals.dim = 1;
+[flags,kv,dim]=ltfatarghelper({'dim'},definput,varargin);
+if(dim>2)
+    error('%s: Multidimensional data is not accepted.',upper(mfilename));
 end
+
+
+JJ = numel(ccell);
+Lc = zeros(JJ,1);
+for jj=1:JJ
+   Lc(jj) =  size(ccell{jj},1);
+end
+
+W = size(ccell{end},2);
+% ALLOCATING OUTPUT
+cvec = zeros(sum(Lc),W);
+% DO THE COPY
+LcStart = 1 + cumsum([0;Lc(1:end-1)]); 
+LcEnd = cumsum(Lc); 
+for jj=1:JJ
+  cvec(LcStart(jj):LcEnd(jj),:) = ccell{jj};
+end
+
+% Reshape back to rows
+if(dim==2)
+    cvec = cvec.';
+end
+
 
 
--- wavelets/wavpack2cell.m
+++ wavelets/wavpack2cell.m
@@ -1,72 +1,65 @@
-function ccell = wavpack2cell(cvec,Lc,varargin)
+function [ccell,dim] = wavpack2cell(cvec,Lc,varargin)
 %WAVPACK2CELL Changes wavelet coefficients storing format
 %   Usage:  
 %          ccell = wavpack2cell(cvec,Lc);
+%          ccell = wavpack2cell(cvec,Lc,dim);
 %
 %   Input parameters:
 %         cvec     : Coefficients in packed format.
 %         Lc       : Vector containing coefficients lengths.
+%         dim      : Identifies dimension along which the data were transformed. 
 %
 %   Output parameters:
-%         ccell    : Coefficients stored in a collumn cell-array.
+%         ccell    : Coefficients stored in a cell-array. Each element is
+%         collum or matrix.
+%         dim      : Return used dim. Usefull as an input of the
+%         complementary function |wavcell2pack|_.
 %
-%   *cvec* is collumn vector or matrix with *W* collumns for multi-channel inputs containing
-%   coefficients in the packed format. Coefficients are stored as folows:
+%   This function transformes coefficients in vector/matrix *cvec* to the
+%   cell array *ccell* of collumn vectors/matrices.
+%   *cvec* is vector or matrix containing coefficients in the packed format.
+%   For dim==1, the coefficients are expected to be stored as collumns:
 %   cvec(1:Lc(1),w) - approximation coefficients at level *J* of the channel *w*,
-%   cvec(1+sum(Lc(1:j-1)):sum(Lc(1:j),w) for *j>1*.
-%
+%   cvec(1+sum(Lc(1:j-1)):sum(Lc(1:j),w) for *j>1*, for dim==2 as rows in
+%   the similar manner. 
+%   The resulting *ccell* has `length(Lc)` elements.
+
 
 if(nargin<2)
     error('%s: Too few input parameters.',upper(mfilename));
 end
 
-definput.flags.format = {'channels','onecol'};
-[flags,kv]=ltfatarghelper({},definput,varargin);
+definput.keyvals.dim = [];
+[flags,kv,dim]=ltfatarghelper({'dim'},definput,varargin);
 
+%If dim is not specified use first non-singleton dimension.
+if(isempty(dim))
+    dim=find(size(cvec)>1,1);
+end
+
+if(dim>2)
+    error('%s: Multidimensional data is not accepted.',upper(mfilename));
+end
+
+if(dim==2)
+    cvec = cvec.';
+end
+
+if(sum(Lc)~=size(cvec,1))
+    error('%s: Sum of elements of Lc is not equal to vector length along dimension %d. Possibly wrong dim?',upper(mfilename),dim);
+end
 
 JJ = length(Lc);
-
-if(flags.do_channels)
-
-   [cLen, W] = size(cvec);
-
-   % ALLOCATING OUTPUT
-   ccell = cell(JJ,1);
-
-   for jj=1:JJ
-      ccell{jj} = zeros(Lc(jj),W);
-   end
-
-   % DO THE COPY
-
-
-   lenSumIdx = 1;
-   lenSum = 0;
-   for jj=1:JJ
-      ccell{jj} = cvec(1+lenSum:Lc(lenSumIdx)+lenSum,:);
-      lenSum = lenSum+Lc(lenSumIdx);
-      lenSumIdx=lenSumIdx+1;
-   end
-
-elseif(flags.do_onecol)
-   JJ = size(Lc,1);
-   ccell = cell(JJ,1);
-   for jj=1:JJ
-      ccell{jj} = zeros(Lc{jj});
-   end
-   
-   lenSumIdx = 1;
-   lenSum = 0;
-   for jj=1:JJ
-      ccell{jj}(:) = cvec(1+lenSum:prod(Lc{lenSumIdx})+lenSum);
-      lenSum = lenSum+prod(Lc{lenSumIdx});
-      lenSumIdx=lenSumIdx+1;
-   end
-   
-    
-else
-    error('Should not get here.');
+% ALLOCATING OUTPUT
+ccell = cell(JJ,1);
+% DO THE COPY
+LcEnd = cumsum(Lc); 
+LcStart = 1 + cumsum([0;Lc(1:end-1)]); 
+for jj=1:JJ
+  ccell{jj} = cvec(LcStart(jj):LcEnd(jj),:);
 end
+
+
 
 
 
