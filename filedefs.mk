e555526f8ed13e3f1b6929bb77c933a0d8e44e7d
prusa <prusa@users.sourceforge.net>
2013-04-02 18:34:40 +0200

    Merged ltfatmex_mingw64 -> ltfatmex. There is no longer need for creating include libs. Makefiles modified accordingly.

--- filedefs.mk
+++ filedefs.mk
@@ -1,11 +1,10 @@
 files = dgt.o dgt_fac.o dgt_fb.o dgt_multi.o dgt_ola.o dgt_shear.o	\
   dgt_walnut.o dgtreal_fac.o dwilt.o filterbank.o heapint.o		\
   idgt_fac.o idgt_fb.o integer_manip.o iwfac.o pfilt.o reassign.o	\
-  spread.o tfutil.o wfac.o windows.o winmanip.o ltfat_blaslapack.o	\
-  gabdual.o gabdual_fac.o gabtight.o wavelets.o \
+  spread.o tfutil.o wfac.o windows.o winmanip.o wavelets.o \
 
 files_blaslapack = \
-	gabdual.o gabtight.o gabdual_fac.o gabtight_fac.o
+	ltfat_blaslapack.o gabdual.o gabtight.o gabdual_fac.o gabtight_fac.o
 
 allfiles = $(files) $(files_blaslapack)
 
