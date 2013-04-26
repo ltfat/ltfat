fe26441338fd530032d41b00c80e83478f2f04a1
prusa <prusa@users.sourceforge.net>
2013-04-26 14:01:59 +0200

    fftreal is now 1.3x faster for for each consecutive call with the same input array length.

--- mex/comp_fftreal.c
+++ mex/comp_fftreal.c
@@ -1,3 +1,6 @@
+#include "fftw3.h"
+#include "stdlib.h"
+
 #ifndef _LTFAT_MEX_FILE
 #define _LTFAT_MEX_FILE
 
@@ -7,6 +10,18 @@
 #define REALARGS
 #define NOCOMPLEXFMTCHANGE
 
+static fftw_plan* p_old = 0;
+
+static void fftrealAtExit(void)
+{
+  if(p_old!=0)
+  {
+     fftw_destroy_plan(*p_old);
+     free(p_old);
+  }
+}
+
+
 #endif // _LTFAT_MEX_FILE - INCLUDED ONCE
 
 #define MEX_FILE __BASE_FILE__
@@ -15,13 +30,21 @@
 #if defined(LTFAT_SINGLE) || defined(LTFAT_DOUBLE)
 #include "ltfat_types.h"
 #include "config.h"
-#include "fftw3.h"
+
 
 // Calling convention:
 //  comp_fftreal(f);
 
+
+
 void LTFAT_NAME(ltfatMexFnc)( int nlhs, mxArray *plhs[],int nrhs, const mxArray *prhs[] )
 {
+  #ifdef LTFAT_DOUBLE
+  if(p_old==0)
+  {
+      mexAtExit(fftrealAtExit);
+  }
+  #endif
 
   int L, W, L2;
   LTFAT_REAL *f, *cout_r, *cout_i;
@@ -62,11 +85,28 @@ void LTFAT_NAME(ltfatMexFnc)( int nlhs, mxArray *plhs[],int nrhs, const mxArray 
   p = LTFAT_FFTW(plan_guru_split_dft_r2c)(1, dims,
 				   1, howmanydims,
 				   f, cout_r, cout_i, FFTW_OPTITYPE);
+  /*
+  FFTW documentation qote http://www.fftw.org/fftw3_doc/New_002darray-Execute-Functions.html#New_002darray-Execute-Functions:
+  ...
+  creating a new plan is quick once one exists for a given size
+  ...
+  so why not to store the old plan..
+  */
+
+
+  if(p_old!=0)
+  {
+    fftw_destroy_plan(*p_old);
+    free(p_old);
+  }
+  p_old = malloc(sizeof(p));
+  memcpy(p_old,&p,sizeof(p));
+
 
   // Real FFT.
   LTFAT_FFTW(execute)(p);
 
-  LTFAT_FFTW(destroy_plan)(p);
+  // LTFAT_FFTW(destroy_plan)(p);
 
   return;
 }
