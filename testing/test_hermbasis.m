684d48698d9207a9d15b7de5961291faba0311bd
Peter L. Søndergaard <soender@users.sourceforge.net>
2013-04-30 10:40:45 +0200

    updated test_hermbasis

--- testing/test_hermbasis.m
+++ testing/test_hermbasis.m
@@ -1,6 +1,8 @@
-Lr=[9,10,12];
+Lr=[100,101,102,103];
 
 test_failed=0;
+
+highordercoef=0.80;
 
 disp(' ===============  TEST_HERMBASIS ==========');
 
@@ -9,7 +11,9 @@ for ii=1:length(Lr)
     for n=2:4
         
         L=Lr(ii);
-        
+
+        highorder=round(L*highordercoef);
+
         [H,D]=hermbasis(L,n);
         
         r1=(H*H')-eye(L);
@@ -26,17 +30,47 @@ for ii=1:length(Lr)
         
         s=sprintf('HERMBASIS DFT  L:%3i n:%3i %0.5g %s',L,n,res,fail);
         disp(s);
-
-        [H,D]=pherm(L,0:round(L*.8)-1);
+        
+        [H,D]=pherm(L,0:highorder-1,'fast');
 
         res=norm(dft(H)-H*diag(D));
         [test_failed,fail]=ltfatdiditfail(res,test_failed);
         
-        s=sprintf('PHERM     DFT  L:%3i n:%3i %0.5g %s',L,n,res,fail);
-        disp(s);
-
-        
     end;
         
+    s=sprintf('PHERM     DFT  L:%3i %0.5g %s',L,res,fail);
+    disp(s);
+    
+    [H,D]=pherm(L,0:highorder-1,'fast','qr');
+    
+    res=norm(dft(H)-H*diag(D));
+    [test_failed,fail]=ltfatdiditfail(res,test_failed);
+    
+    s=sprintf('PHERM QR  DFT  L:%3i %0.5g %s',L,res,fail);
+    disp(s);
+    
+    r1=(H'*H)-eye(highorder);
+    res=norm(r1,'fro');
+    [test_failed,fail]=ltfatdiditfail(res,test_failed);
+    
+    s=sprintf('PHERM QR orth  L:%3i %0.5g %s',L,res,fail);
+    disp(s);
+    
+    
+    [H,D]=pherm(L,0:highorder-1,'fast','polar');
+    
+    res=norm(dft(H)-H*diag(D));
+    [test_failed,fail]=ltfatdiditfail(res,test_failed);
+    
+    s=sprintf('PHERM POL DFT  L:%3i %0.5g %s',L,res,fail);
+    disp(s);
+    
+    r1=(H'*H)-eye(highorder);
+    res=norm(r1,'fro');
+    [test_failed,fail]=ltfatdiditfail(res,test_failed);
+    
+    s=sprintf('PHERM POL orth L:%3i %0.5g %s',L,res,fail);
+    disp(s);
+            
 end;
 
