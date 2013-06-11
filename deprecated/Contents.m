c6d26a7b1941b6f9e60960ef12873479d3d3e19a
Peter L. Søndergaard <soender@users.sourceforge.net>
2013-06-11 13:02:47 +0200

    Added missing Contents.m in deprecated/

---
+++ deprecated/Contents.m
@@ -0,0 +1,21 @@
+% LTFAT - Deprecated functions
+%
+%  Peter L. Søndergaard, 2012 - 2013.
+%
+%  The functions in the directory has been deprecated, and their use is
+%  no longer recommended. The help of each function provides a
+%  description on how to replace the function.
+%  
+%  Deprecated functions
+%    GABELITISTLASSO   - Use |franagrouplasso|  
+%    GABGROUPLASSO     - Use |franagrouplasso|
+%    GABLASSO          - Use |franalasso|
+%    GABMULEIGS        - Use |framemuleigs|
+%    GABMUL            - Use |framemul|
+%    IUFILTERBANK      - Use |ifilterbank|
+%    IUNSDGT           - Use |insdgt|
+%    IUNSDGTREAL       - Use |insdgtreal|
+%    TFMAT             - Use |framematrix| or |operatormatrix|
+%
+%  For help, bug reports, suggestions etc. please send email to
+%  ltfat-help@lists.sourceforge.net
