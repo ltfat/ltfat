b0d6e225d479834f88b2c4d3808075ced3e3a325
prusa <prusa@users.sourceforge.net>
2013-06-12 12:42:06 +0200

    Cleanup, minor tweaks, added instructions for compilation on Windows (32bit and 64bit).

--- blockproc/blockprocinit.m
+++ blockproc/blockprocinit.m
@@ -1,1 +1,12 @@
 status=1;
+
+% Add entry to the dynamic classpath if JVM is present.
+if ~isempty(which('javaaddpath')) 
+   try
+      javaaddpath([basepath,filesep,'blockproc',filesep,'java',filesep]);
+   catch err
+      warning('%s: JVM support not present.',upper(mfilename));
+   end
+else
+   warning('%s: Java toolbox not present.',upper(mfilename));
+end
