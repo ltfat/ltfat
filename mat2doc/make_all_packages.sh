036ffeb8fa1c6dbb28d5e3e39d65bb946d85466e
prusa <zdenek.prusa@gmail.com>
2013-08-01 21:59:35 +0200

    Some polishing of the demos.

--- mat2doc/make_all_packages.sh
+++ mat2doc/make_all_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mat2doc `pwd`/.. mat --script=release.py --tgz 
-mat2doc `pwd`/.. mat --script=release.py --zip --dos --encoding=windows-1252 --addon=ltfat-win64 --packagename=ltfat-win64
-mat2doc `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_maci --packagename=ltfat-maci
-mat2doc `pwd`/.. mat --script=release.py --octpkg
+mat2doc.py `pwd`/.. mat --script=release.py --tgz 
+mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --encoding=windows-1252 --addon=ltfat_win64 --packagename=ltfat-win64
+mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_maci --packagename=ltfat-maci
+mat2doc.py `pwd`/.. mat --script=release.py --octpkg
 
--- mat2doc/startup.m
+++ mat2doc/startup.m
@@ -1,6 +1,6 @@
-addpath ~/nw/ltfat
-addpath ~/toolboxes/minFunc
-addpath ~/nw/unlocbox/
+addpath ~/ltfat
+addpath ~/dev/minFunc/minFunc/
+addpath ~/dev/unlocbox/
 
 ltfatstart;
 init_unlocbox;
