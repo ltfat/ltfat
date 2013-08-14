797cbb4243b07dc23ec00245cc21ac3417b69ec1
prusa <prusa@users.sourceforge.net>
2013-08-14 11:05:51 +0200

    Fix in wfilt_matlabwtwrapper

--- wavelets/wfilt_matlabwtwrapper.m
+++ wavelets/wfilt_matlabwtwrapper.m
@@ -1,4 +1,4 @@
-function [h,g,a] = wfilt_matlabwtwrapper(wname)
+function [h,g,a,info] = wfilt_matlabwtwrapper(wname)
 %WFILT_MATLABWTWRAPPER Wrapper of the Matlab Wavelet Toolbox wfilters function
 %   Usage: [h,g,a] = wfilt_matlabwtwrapper(wname);
 %
@@ -7,16 +7,20 @@ function [h,g,a] = wfilt_matlabwtwrapper(wname)
 %   requires the Matlab Wavelet Toolbox.
 %
 
-a = [2,2];
+a = [2;2];
 [lo,hi,lo_s,hi_s] = wfilters(wname);
 
 h=cell(2,1);
-h{1} = lo;
-h{2} = hi;
-    
+h{1} = lo(:);
+h{2} = hi(:);
 
-if(nargout>1)
-    g=cell(2,1);
-    g{1} = lo_s;
-    g{2} = hi_s;
+g=cell(2,1);
+g{1} = flipud(lo_s(:));
+g{2} = flipud(hi_s(:));
+
+if all(h{1}==g{1}) && all(h{2}==g{2})
+  info.istight = 1;
+else
+  info.istight = 0; 
 end
+
