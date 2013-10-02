6198703462e1f0ec598e3f1d797ae2809f5c8f9b
prusa <prusa@users.sourceforge.net>
2013-10-02 19:10:28 +0200

    Added ifilterbank_* as MEX functions.

--- ltfat_typeindependent.h
+++ ltfat_typeindependent.h
@@ -203,6 +203,29 @@ LTFAT_H_NAME(convsub_fftbl_plan)(const LTFAT_H_COMPLEXH *F, const size_t L,
                             const double a, const double realonly, LTFAT_H_COMPLEXH *cout,
                             LTFAT_H_FFTW(plan) *p);
 
+// Inverse
+LTFAT_EXTERN void
+LTFAT_H_NAME(upconv_fft)(const LTFAT_H_COMPLEXH *c, const size_t Lc,
+                         const LTFAT_H_COMPLEXH *G, const size_t a,
+                         LTFAT_H_COMPLEXH *Fout);
+
+LTFAT_EXTERN void
+LTFAT_H_NAME(upconv_fft_plan)(const LTFAT_H_COMPLEXH *c, const size_t Lc,
+                              const LTFAT_H_COMPLEXH *G, const size_t a,
+                              LTFAT_H_COMPLEXH *Fout, LTFAT_H_FFTW(plan) *p,
+                              LTFAT_H_COMPLEXH *cbuffer);
+
+LTFAT_EXTERN void
+LTFAT_H_NAME(upconv_fftbl)(const LTFAT_H_COMPLEXH *c, const size_t Lc,
+                            const LTFAT_H_COMPLEXH *G, const size_t Lg, const int foff,
+                            const double a, const double realonly, LTFAT_H_COMPLEXH *Fout);
+
+LTFAT_EXTERN void
+LTFAT_H_NAME(upconv_fftbl_plan)(const LTFAT_H_COMPLEXH *c, const size_t Lc,
+                            const LTFAT_H_COMPLEXH *G, const size_t Lg, const int foff,
+                            const double a, const double realonly, LTFAT_H_COMPLEXH *Fout,
+                            LTFAT_H_FFTW(plan) *p,LTFAT_H_COMPLEXH *cbuffer);
+
 
 
 /* -------- windows ------------------------------ */
