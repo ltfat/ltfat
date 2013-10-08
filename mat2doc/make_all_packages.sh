47dcd75e7672d50a77a2e3c17a9cf523d74eac05
prusa <prusa@users.sourceforge.net>
2013-10-08 18:49:28 +0200

    Modified arguments of mat2doc in make_all_packages.

--- mat2doc/make_all_packages.sh
+++ mat2doc/make_all_packages.sh
@@ -4,7 +4,7 @@ mat2doc.py `pwd`/.. mat --script=release.py --tgz
 
 # The command below chokes on umlauts in blockana
 #mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --encoding=windows-1252 --addon=ltfat_win64 --packagename=ltfat-win64
-mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_win64 --packagename=ltfat-win64
-mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_maci --packagename=ltfat-maci
+mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_win64 --packagename=ltfat-%s-win64
+mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_maci --packagename=ltfat-%s-maci
 mat2doc.py `pwd`/.. mat --script=release.py --octpkg
 echo Remember to push to sourceforge for the Octave package
