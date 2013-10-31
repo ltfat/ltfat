ef4b249d4f63dbacb5e5d120c9c5ee6c4fad992c
Peter L. Søndergaard <soender@users.sourceforge.net>
2013-10-31 14:48:02 +0100

    removed the checking of fftw header files

--- mat2doc/make_all_packages.sh
+++ mat2doc/make_all_packages.sh
@@ -7,4 +7,5 @@ mat2doc.py `pwd`/.. mat --script=release.py --tgz
 mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_win64 --packagename=ltfat-%s-win64
 mat2doc.py `pwd`/.. mat --script=release.py --zip --dos --addon=ltfat_maci --packagename=ltfat-%s-maci
 mat2doc.py `pwd`/.. mat --script=release.py --octpkg
-echo Remember to push to sourceforge for the Octave package
+echo "Remember to push to sourceforge for the Octave package:"
+echo "---> git push ssh://soender@git.code.sf.net/p/octave/ltfat master"
