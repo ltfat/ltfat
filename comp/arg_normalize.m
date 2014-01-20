3e30ee272e192227518a08a178898ec1272c8981
prusa <prusa@users.sourceforge.net>
2014-01-20 14:23:32 +0100

    Forgotten arg_ file for normalize.

--- comp/arg_normalize.m
+++ comp/arg_normalize.m
@@ -1,5 +1,6 @@
 function definput=arg_normalize(definput)
   
-  definput.flags.norm={'2','1','inf','area','energy','peak','s0','rms','null'};
+  definput.flags.norm={'2','1','inf','area','energy','peak',...
+                       's0','rms','null','wav'};
 
 
