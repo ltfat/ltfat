8620c4098209be5cd5ba3edcee1bc99ef953f4dc
Zdenek Prusa <zdenek.prusa@gmail.com>
2014-07-03 17:15:18 +0200

    Added function for computing periodic group delay

--- fourier/pgrpdelay.m
+++ fourier/pgrpdelay.m
@@ -1,11 +1,39 @@
-function tgrad = pgrpdelay(g,L,varargin)
+function ggd = pgrpdelay(g,L)
+%PGRPDELAY Group delay of a filter with periodic boundaries
+%   Usage:  ggd = pgrpdelay(g,L);
+%           ggd = pgrpdelay(g,L,'plot');
+%
+%   `pgrpdelay(g,L)` computes group delay of filter *g* as a negative
+%   derivative of the phase frequency response of filter *g* assuming
+%   periodic (cyclic) boundaries i.e. the delay may be a negative number.
+%   The derivative is calculated using the second order centered difference
+%   approximation. 
+%   The resulting group delay is in samples.
+%
+%   Example:
+%   --------
+%
+%   The following example shows a group delay of causal, moving average
+%   6tap FIR filter and it's magnitude frequency response for comparison.
+%   The dips in the group delay correspond to places where modulus of the 
+%   frequency response falls to zero.:::
+%
+%      g = struct(struct('h',ones(6,1),'offset',0)); 
+%      L = 512;
+%      figure(1);
+%      subplot(2,1,1);
+%      plot(-L/2+1:L/2,fftshift(pgrpdelay(g,512)));
+%      axis tight;ylim([0,4]);
+%
+%      subplot(2,1,2);
+%      magresp(g,L,'nf');
+%      
 
 
-[g,info] = comp_fourierwindow(g,L,upper(mfilename));
+g = comp_fourierwindow(g,L,upper(mfilename));
 
-H = (comp_transferfunction(g,L));
+H = comp_transferfunction(g,L);
 Harg = angle(H);
-Hmod = abs(H);
 
 % Forward approximation
 tgrad_1 = Harg-circshift(Harg,-1);
@@ -14,9 +42,7 @@ tgrad_1 = tgrad_1 - 2*pi*round(tgrad_1/(2*pi));
 tgrad_2 = circshift(Harg,1)-Harg;
 tgrad_2 = tgrad_2 - 2*pi*round(tgrad_2/(2*pi));
 % Average
-tgrad = (tgrad_1+tgrad_2)/2;
+ggd = (tgrad_1+tgrad_2)/2;
  
-tgrad = tgrad/(2*pi)*L;
+ggd = ggd/(2*pi)*L;
 
-
-%plot(tgrad);
