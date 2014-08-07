bc8666b0bce29b6d4ccf8d47ccc4df2ce305a25c
Zdenek Prusa <zdenek.prusa@gmail.com>
2014-08-07 19:44:18 +0200

    Automatically removing 'src/autom4te.cache' dir when doing release

--- mat2doc/mat/release.py
+++ mat2doc/mat/release.py
@@ -23,6 +23,10 @@ for root, dirs, files in os.walk(conf.t.dir, topdown=False):
 # "bootstrap" the configure files
 os.system("cd "+conf.t.dir+"/src; ./bootstrap")
 
+s=os.path.join(conf.t.dir,'src','autom4te.cache')
+rmrf(s)
+os.rmdir(s)
+
 # Compile the Java classes
 os.system("cd "+conf.t.dir+"/blockproc/java; make")
 os.system("cd "+conf.t.dir+"/blockproc/java; make classclean")
