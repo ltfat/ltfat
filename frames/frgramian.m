36f5825dde93496b8ecfc9dbbd19d98decdb116f
Zdenek Prusa <zdenek.prusa@gmail.com>
2014-10-16 20:11:03 +0200

    Small changes in frgrammian.m

--- frames/frgramian.m
+++ frames/frgramian.m
@@ -1,4 +1,4 @@
-function o = frgramian(c, Fa, Fs);
+function o = frgramian(c, Fa, Fs)
 %FRGRAMIAN Frame Gramian operator
 %   Usage:  o=frgramian(c, F);
 %           o=frgramian(c, Fa, Fs);
@@ -11,27 +11,30 @@ function o = frgramian(c, Fa, Fs);
 %   Output parameters: 
 %          o    : Output coefficients
 %     
-%   `o=frgramian(c,F)` computes the Gramian operator or Gram matrix of the 
+%   `o=frgramian(c,F)` applies the Gramian operator or Gram matrix of the 
 %   frame *F*. The entries of the Gram matrix are the inner products of the 
 %   frame elements of *F*. The frame must have been created using |frame|.
 %   If the frame *F* is a Parseval frame, the Gramian operator is a projection 
 %   onto the range of the frame analysis operator.
 %
-%   `o=frgramian(o, Fa, Fs)` computes the Gramian operator with the frames *Fa* 
-%   and *Fs*. Here is *Fs* the frame associated with the frame synthesis operator 
-%   and *Fa* the frame that is associated with the frame analysis operator. The 
-%   entries of the matrix that is constructed through the Gramian operator are the 
-%   inner products of the frame elements of *Fa* and *Fs*. The frames *Fa* and 
-%   *Fs* must have been created using |framepair|.
-%   If *Fa* and *Fs* are canonical dual frames, the Gramian operator is a projection
-%   onto the range of the frame analysis operator.
+%   `o=frgramian(c, Fa, Fs)` applies the (cross) Gramian operator with the 
+%   frames *Fa* and *Fs*. Here *Fs* is the frame associated with the frame
+%   synthesis operator and *Fa* the frame that is associated with the 
+%   frame analysis operator. The entries of the matrix that is constructed
+%   through the Gramian operator are the inner products of the frame 
+%   elements of *Fa* and *Fs*.
+%   If *Fa* and *Fs* are canonical dual frames, the Gramian operator is a 
+%   projection onto the range of the frame analysis operator.
 %
 % AUTHOR: Jordy van Velthoven
 
 complainif_notenoughargs(nargin, 2, 'FRGRAMIAN');
+complainif_notvalidframeobj(Fa,'FRGRAMIAN');
 
 if (nargin == 2)
-  Fs = Fa;
+   Fs = Fa;
+else
+   complainif_notvalidframeobj(Fs,'FRGRAMIAN'); 
 end;
 
 o = frana(Fa, frsyn(Fs, c));
