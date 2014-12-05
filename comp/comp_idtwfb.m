c79571f96d7cd31b003d805bce4395c2d24e9813
Zdenek Prusa <zdenek.prusa@gmail.com>
2014-12-05 07:02:44 +0100

    A sligh rearangement of operations in comp_idtwfb.m
    Such that it better reflects the algorithm description in the
    forthcoming paper.

--- comp/comp_idtwfb.m
+++ comp/comp_idtwfb.m
@@ -3,9 +3,9 @@ function f = comp_idtwfb(c,nodes,dualnodes,Lc,rangeLoc,rangeOut,ext,do_complex)
 
 if do_complex
    % Split the coefficients
-   c1 = cellfun(@(cEl1,cEl2) cEl1 + cEl2, c(1:end/2),c(end:-1:end/2+1),...
+   c1 = cellfun(@(cEl1,cEl2) (cEl1 + cEl2)/2, c(1:end/2),c(end:-1:end/2+1),...
              'UniformOutput',0);
-   c2 = cellfun(@(cEl1,cEl2) -1i*cEl1 + 1i*cEl2, c(1:end/2),c(end:-1:end/2+1),...
+   c2 = cellfun(@(cEl1,cEl2) (-1i*cEl1 + 1i*cEl2)/2, c(1:end/2),c(end:-1:end/2+1),...
              'UniformOutput',0);
 else
    c1 = cellfun(@real,c,'UniformOutput',0);
@@ -14,12 +14,9 @@ end
 
 
 f1 = comp_iwfbt(c1,nodes,Lc,rangeLoc,rangeOut,ext);
-f2 = comp_iwfbt(c2,dualnodes,Lc,rangeLoc,rangeOut,ext); 
+f = f1 + comp_iwfbt(c2,dualnodes,Lc,rangeLoc,rangeOut,ext); 
 
-
-if do_complex
-   f = (f1+f2)/2;
-else
-   f = real(f1+f2);
+if ~do_complex
+   f = real(f);
 end
     
