9c3b39597f71d3530cf2cd8e0095badf8fa47d37
Zdenek Prusa <zdenek.prusa@gmail.com>
2015-01-22 18:45:41 +0100

    Fixed ref_dft
    So the test_purefreq no longer fails.

--- reference/ref_dft.m
+++ reference/ref_dft.m
@@ -6,15 +6,18 @@ function c=ref_dft(f)
 %
 %   AUTHOR: Jordy van Velthoven
 
-L = length(f);
-c = zeros(L,1);
+L=size(f,1);
+W=size(f,2);
+c= zeros(L,W);
 
+for w=1:W
 for k=0:L-1
   for l=0:L-1
-    c(k+1) = c(k+1) + f(l+1) * exp(-2*pi*i*k*l/L);
+    c(k+1,w) = c(k+1,w) + f(l+1,w) * exp(-2*pi*i*k*l/L);
   end;
 end;
+end
 
-c = c/sqrt(L);
+c = c./sqrt(L);
 
 
