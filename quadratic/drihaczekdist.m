d452676f16804def5ff46322ce6d4f2b0a17f19f
Jordy van Velthoven <jt.vanvelthoven@gmail.com>
2015-02-04 08:34:39 +0100

    Added warning of redundancy to drihaczekdist.

--- quadratic/drihaczekdist.m
+++ quadratic/drihaczekdist.m
@@ -9,7 +9,11 @@ function r = drihaczekdist(f);
 %   .. math:: r\left( k+1,\; l+1 \right)\; =\; f\left( l+1 \right)\; \overline{c\left( k+1 \right)}e^{-2\pi ikl/L}
 %
 %   where $k, l=0,\ldots,L-1$ and $c$ is the Fourier transform of $f$.
-
+%
+%   **WARNING**: The quadratic time-frequency distributions are highly 
+%   redundant. For an input vector of length L, the quadratic time-frequency
+%   distribution will be a $L \times L$ matrix.
+%
 %   AUTHOR: Jordy van Velthoven
 %   TESTING: TEST_DRIHACZEKDIST
 %   REFERENCE: REF_DRIHACZEKDIST
