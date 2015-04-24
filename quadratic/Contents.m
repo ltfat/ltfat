7a3c602c4ec52f22c842b1b13a99ab9d08fd6114
Jordy van Velthoven <jt.vanvelthoven@gmail.com>
2015-04-24 11:06:35 +0200

    Added contents file for quadratic subdirectory.

---
+++ quadratic/Contents.m
@@ -0,0 +1,12 @@
+% LTFAT - Quadratic time-frequency distributions
+%
+%   Jordy van Velthoven, 2014 - 2015.
+%
+%	Quadratic distributions
+%    AMBIGUITYFUNCTION  -  Ambiguity function
+%	 WIGNERVILLEDIST	-  Wigner-Ville distribution
+%	 DRIHACZEKDIST		-  Discrete Rihaczek distribution
+%
+%	Plotting routine
+%	 PLOTQUADTFDIST		- Plot quadratic time-frequency distribution
+%
