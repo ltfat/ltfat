aca599ef2349f47f15ecebf36106414a8fe94f59
Zdenek Prusa <zdenek.prusa@gmail.com>
2015-06-23 20:47:57 +0200

    New tests for gabphasederiv

--- testing/test_gabphasederiv.m
+++ testing/test_gabphasederiv.m
@@ -1,7 +1,10 @@
-function test_failed = test_gabphasederiv(dflag,varargin)
+function test_failed = test_gabphasederiv()
 test_failed = 0;
 
-L = 1000;
+% Tests whether all the algorithms return the same values
+disp('-----------------test_gabphasederiv----------------------');
+
+L = 100;
 l = 0:L-1; l = l(:);
 
 f1 = exp(1i*pi*0.1*l);
@@ -9,79 +12,53 @@ f2 = pchirp(L,1);
 f3 = expchirp(L,0.2,2.2,'fc',-1.2);
 f4 = exp(1i*pi*0.5*L*(l./(L)).^2);
 f5 = exp(1i*pi*0.1*L*(l./(L)).^2);
-tfr = 1;
-g = {'gauss',tfr};
+f6 = zeros(L,1); f6(floor(L/2):floor(L/2)+5) = 1;
 
+f = {f2,f1,f2};
 
-a = 1;
-%M = L/1;
-M = 1000;
-minlvl = 1e-4;
+a = [4,4,4];
+M = [16,8,8];
+%g = {{'gauss',1},'gauss',{'hann',8}};
+g = {{'gauss',1}};
 
+phaseconvCell = {'freqinv','timeinv','symphase','relative'};
 
-f = f3;
-sgram(f,'dynrange',90);
-%f = dft(f);
+dflags = {'t','f','tt','ff','tf','ft'};
 
+for ii =1:numel(g)
+    for phaseconvId=1:numel(phaseconvCell)
+        phaseconv=phaseconvCell{phaseconvId};
+        for dflagId = 1:numel(dflags)
+            dflag = dflags{dflagId};
+            
+            c = dgt(f{ii},g{ii},a(ii),M(ii));
+            [~,info]=gabwin(g{ii},a(ii),M(ii),dgtlength(numel(f{ii}),a(ii),M(ii)));
+            
+            minlvl = max(abs(c(:)))*1e-4;
+            algArgs = {
+                {'dgt',f{ii},g{ii},a(ii),M(ii)}
+                {'phase',angle(c),a(ii)}
+                {'cross',f{ii},g{ii},a(ii),M(ii)} 
+                {'abs',abs(c),g{ii},a(ii)}   
+                };
 
-
-c = dgt(f,g,a,M);
-
-dynrange = 40;
-dynrangerat = 10^(dynrange/20)
-mincoef = max(abs(c(:)))/dynrangerat;
-
-phase = unwrap(angle(c),[],2);
-phaselocked = (angle(phaselock(c,a)));
-phase(abs(c)<mincoef) = 0;
-phaselocked(abs(c)<mincoef) = 0;
-%  figure(10);
-%  plotdgt(phase/pi,a,'lin','clim',[-pi,pi]);
-%   figure(11);
-%   plotdgt(phaselocked/pi,a,'lin','clim',[-pi,pi]);
-
-clim = [-1,1];
-
-figure(1);
-phasedt2 = gabphasederiv(dflag,'dgt',f,g,a,M,'minlvl',minlvl,varargin{:});
-phasedt2(abs(c)<mincoef) = 0;
-plotdgt(phasedt2,a,'lin','clim',clim);
-
-% figure(2);
-% phasedf2 = gabphasederiv(dflag,'abs',abs(c),g,a,varargin{:});
-% phasedf2(abs(c)<mincoef) = 0;
-% plotdgt(phasedf2,a,'lin','clim',clim);
-
-figure(3);
-phasedff2 = gabphasederiv(dflag,'phase',angle(c),a,varargin{:});
-phasedff2(abs(c)<mincoef) = 0;
-plotdgt(phasedff2,a,'lin','clim',clim);
-
-figure(4);
-phasedt2 = gabphasederiv(dflag,'cross',f,g,a,M,'minlvl',minlvl,varargin{:});
-phasedt2(abs(c)<mincoef) = 0;
-plotdgt(phasedt2,a,'lin','clim',clim);
-
-%figure(4);
-%plotdgt(abs(phasedt2-phasedff2),a,'lin','clim',[0,50]);
-
- figure(5);
- plotdgt(c,a,'dynrange',dynrange);
- 
-% 
- figure(6);
- tt = gabphasederiv('tt','dgt',f,g,a,M,'minlvl',minlvl);
- tf = -gabphasederiv('tf','dgt',f,g,a,M,'minlvl',minlvl,'freqinv');
- 
- is = tt./tf;
- is(abs(c)<mincoef) = 0;
- plotdgt(is,a,'lin','clim',clim);
- 
- figure(7);
- tf = gabphasederiv('tf','dgt',f,g,a,M,'minlvl',minlvl,'timeinv');
- ff = -gabphasederiv('ff','dgt',f,g,a,M,'minlvl',minlvl);
- 
- is = tf./ff;
- is(abs(c)<mincoef) = 0;
- plotdgt(is,a,'lin','clim',clim);
-
+            algRange = 1:numel(algArgs);
+            if ~info.gauss
+                algRange = 1:numel(algArgs)-1;
+            end
+            pderivs = cell(numel(algRange),1);
+            
+            for algId=algRange
+                alg = algArgs{algId};
+                pderivs{algId}=gabphasederiv(dflag,alg{:},phaseconv);
+                % Mask by big enough coefficients
+                pderivs{algId}(abs(c)<minlvl)=0;
+            end
+            % MSE
+            res = (cellfun(@(pEl) norm(pEl(:)-pderivs{1}(:)).^2/numel(pEl(:)),pderivs(2:end)));
+            [test_failed,failstring]=ltfatdiditfail( sum(res),...
+                           test_failed,1e-8);
+             fprintf('GABPHASEDERIV dflag:%2s phaseconv:%8s %d %s\n',dflag,phaseconv,sum(res),failstring);
+        end
+    end
+end
