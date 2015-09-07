e4eb21e7e5713f8784fde1e1c2a6efd85a29a52e
Thibaud Necciari <thibaud@kfs.oeaw.ac.at>
2015-09-07 15:43:15 +0200

    "Flag added to audfiltbw.m to obtain the bandwidth on the Bark scale."

--- auditory/audfiltbw.m
+++ auditory/audfiltbw.m
@@ -1,34 +1,54 @@
-function bw = audfiltbw(fc)
+function bw = audfiltbw(fc,varargin)
 %AUDFILTBW  Bandwidth of auditory filter
 %   Usage: bw = audfiltbw(fc)
 %
-%   `audfiltbw(fc)` returns the equivalent rectangular bandwidth of the
-%   auditory filter at center frequency *fc*. The function uses the
-%   relation
+%   `audfiltbw(fc)` returns the critical bandwidth of the auditory filter 
+%   at center frequency *fc* defined in equivalent rectangular bandwidth.
+%   The function uses the relation
 %
 %   .. bw = 24.7 + fc/9.265
 %
 %   .. math::  bw = 24.7 + \frac{fc}{9.265}
 %     
-%   as estimated in Glasberg and Moore (1990)
+%   as estimated in Glasberg and Moore (1990).
+% 
+%  `audfiltbw(fc,'bark') returns the critical bandwidth at *fc* according to
+%   the Bark scale using the relation:
+% 
+%       bw = 25 + 75 ( 1+1.4\times10^{-6} fc^2 )^{0.69}.
+% 
+% as estimated by Zwicker and Terhardt (1980).
 %
 %   See also: freqtoerb, erbspace
 %
-%   References: glasberg1990daf
+%   References: glasberg1990daf,
+%       Zwicker, E., and Terhardt, E. (1980). "Analytical expressions for critical-
+%           band rate and critical bandwidth as a function of frequency,"
+%           J. Acoust. Soc. Am. 68, 1523-1524.
   
 %   AUTHOR : Peter L. Søndergaard
   
 % ------ Checking of input parameters ---------
   
-error(nargchk(1,1,nargin));
+% error(nargchk(1,1,nargin));
+if nargin<1
+  error('%s: Too few input parameters.',upper(mfilename));
+end;
 
 if ~isnumeric(fc) || any(fc(:)<0)
   error('AUDFILTBW: fc must be non-negative.');
 end;
 
+definput.flags.audscale={'erb','bark'};
+[flags,kv]=ltfatarghelper({},definput,varargin);
+
 % ------ Computation --------------------------
 
 % FIXME: What is the upper frequency for which the estimation is valid?
+if flags.do_erb
+    bw = 24.7 + fc/9.265;
+end
 
-bw = 24.7 + fc/9.265;
-
+if flags.do_bark
+    bw = 25 + 75*(1 + 1.4E-6*fc^2)^0.69;
+end
