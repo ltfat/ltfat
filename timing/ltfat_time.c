be9b369525c9b6b0a8b409044686256836666166
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-02-10 16:11:29 +0100

    New timer in timing/ltfat_time.c
    timespec_get from C11

--- timing/ltfat_time.c
+++ timing/ltfat_time.c
@@ -6,75 +6,88 @@ gettimeofday in MinGW has too coarse resolution. See
 https://sourceforge.net/p/mingw/bugs/1821/
 */
 #if defined(_WIN32) || defined(__WIN32__)
-#include <windows.h> 
+#include <windows.h>
 
 double ltfat_time()
 {
-   LARGE_INTEGER frequency;
-   LARGE_INTEGER t1;
-   QueryPerformanceFrequency(&frequency);
-   QueryPerformanceCounter(&t1);
-   
-   return (t1.QuadPart) * 1000.0 / frequency.QuadPart;
+    LARGE_INTEGER frequency;
+    LARGE_INTEGER t1;
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&t1);
+
+    return (t1.QuadPart) * 1000.0 / frequency.QuadPart;
 }
+#else
+#if __STDC_VERSION__ >= 201112L
+#include <sys/time.h>
+
+double ltfat_time()
+{
+    struct timespec ts;
+    timespec_get(&ts, TIME_UTC);
+
+    return (1000.0 * ((double)tv.tv_sec) + ((double)tv.tv_nsec) / 1000000.0);
+}
+
 #else
 #include <sys/time.h>
 
 double ltfat_time()
 {
-   struct timeval tv;
-   
-   gettimeofday(&tv, NULL);
+    struct timeval tv;
 
-   //printf("  %lld  %lld  \n",tv.tv_sec,tv.tv_usec);*/
-   
-   return (1000.0*((double)tv.tv_sec)+((double)tv.tv_usec)/1000.0);
+    gettimeofday(&tv, NULL);
+
+    return (1000.0 * ((double)tv.tv_sec) + ((double)tv.tv_usec) / 1000.0);
 }
-
+#endif
 #endif
 
 /*
 Fills array with pseudorandom values in range [0-1]
 */
-void fillRand_d(double *f, int L)
+void fillRand_d(double* f, int L)
 {
-   srand (time(NULL));
-   for(int m=0;m<L;m++)
-   {
-      f[m]=((double) (rand()))/RAND_MAX;
-   }
+    srand (time(NULL));
+
+    for (int m = 0; m < L; m++)
+    {
+        f[m] = ((double) (rand())) / RAND_MAX;
+    }
 }
 
-void fillRand_s(float *f, int L)
+void fillRand_s(float* f, int L)
 {
-   srand (time(NULL));
-   for(int m=0;m<L;m++)
-   {
-      f[m]=((float) (rand()))/RAND_MAX;
-   }
+    srand (time(NULL));
+
+    for (int m = 0; m < L; m++)
+    {
+        f[m] = ((float) (rand())) / RAND_MAX;
+    }
 }
 
-void fillRand_cd(double _Complex *f, int L)
+void fillRand_cd(double _Complex* f, int L)
 {
-   double (*fTmp)[2]  = (double (*)[2]) f;
-   srand (time(NULL));
-   for(int m=0;m<L;m++)
-   {
-      fTmp[m][0]=((double) (rand()))/RAND_MAX;
-	  fTmp[m][1]=((double) (rand()))/RAND_MAX;
-   }
+    double (*fTmp)[2]  = (double (*)[2]) f;
+    srand (time(NULL));
+
+    for (int m = 0; m < L; m++)
+    {
+        fTmp[m][0] = ((double) (rand())) / RAND_MAX;
+        fTmp[m][1] = ((double) (rand())) / RAND_MAX;
+    }
 }
 
-void fillRand_cs(float _Complex *f, int L)
+void fillRand_cs(float _Complex* f, int L)
 {
-   float (*fTmp)[2] = (float (*)[2]) f;
-   srand (time(NULL));
-   for(int m=0;m<L;m++)
-   {
-      fTmp[m][0]=((float) (rand()))/RAND_MAX;
-	  fTmp[m][1]=((float) (rand()))/RAND_MAX;
-   }
-}
+    float (*fTmp)[2] = (float (*)[2]) f;
+    srand (time(NULL));
 
+    for (int m = 0; m < L; m++)
+    {
+        fTmp[m][0] = ((float) (rand())) / RAND_MAX;
+        fTmp[m][1] = ((float) (rand())) / RAND_MAX;
+    }
+}
 
 
