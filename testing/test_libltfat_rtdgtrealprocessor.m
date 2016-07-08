85594b6f804d6dbc4f39a148b7ffc2f3656e3787
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-07-08 10:58:13 +0200

    Updated test for rtdgtprocessor

--- testing/test_libltfat_rtdgtrealprocessor.m
+++ testing/test_libltfat_rtdgtrealprocessor.m
@@ -5,19 +5,23 @@ LTFAT_FIRWIN = enuminfo.LTFAT_FIRWIN;
 gl = 1024;
 M = gl;
 a = 100;
-W = 1;
+W = 2;
 
 gPtr = libpointer('doublePtr',zeros(gl,1));
 gdPtr = libpointer('doublePtr',zeros(gl,1));
 
-calllib('libltfat','firwin_d',LTFAT_FIRWIN.LTFAT_HAMMING,gl,gPtr);
-calllib('libltfat','gabdual_painless_d',gPtr,gl,a,M,gdPtr);
+calllib('libltfat','ltfat_firwin_d',LTFAT_FIRWIN.LTFAT_HAMMING,gl,gPtr);
+calllib('libltfat','ltfat_gabdual_painless_d',gPtr,gl,a,M,gdPtr);
 
-processorPtr = calllib('libltfat','rtdgtreal_processor_wininit_d',...
-    LTFAT_FIRWIN.LTFAT_HAMMING,gl,a,M,W,libpointer('voidPtr'),libpointer('voidPtr'));
+plan = libpointer();
+
+calllib('libltfat','ltfat_rtdgtreal_processor_wininit_d',...
+    LTFAT_FIRWIN.LTFAT_HAMMING,gl,a,M,W,libpointer('voidPtr'),libpointer('voidPtr'),...
+    plan);
 
 
 [bufIn,fs] = gspi;
+bufIn = [bufIn, 10*bufIn];
 %bufIn = ones(1,1000);
 bufOut = zeros(size(bufIn));
 bufLen = 16;
@@ -29,20 +33,20 @@ bufInPtr = libpointer('doublePtr',buf);
 bufOutPtr = libpointer('doublePtr',zeros(size(buf)));
 
 % Matlab automatically converts Ptr to PtrPtr
-calllib('libltfat','rtdgtreal_processor_execute_d',processorPtr,bufInPtr,bufLen,W,bufOutPtr);
+calllib('libltfat','ltfat_rtdgtreal_processor_execute_compact_d',plan,bufInPtr,bufLen,W,bufOutPtr);
 
 bufOut(slice,:) = bufOutPtr.Value;
 
 end
 
 inshift = circshift(bufIn,(gl-1));
-inshift(1:(gl-1)) = 0;
+inshift(1:(gl-1),:) = 0;
 plotthat = [bufOut- inshift];
 plotthat(end-(gl-1):end) = 0;
 stem(plotthat);shg;
 %soundsc(bufOut,fs);
 
-calllib('libltfat','rtdgtreal_processor_done_d',processorPtr);
+calllib('libltfat','ltfat_rtdgtreal_processor_done_d',plan);
 
 
 
