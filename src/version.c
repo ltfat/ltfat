13720a646b3bd26b99c959c1320dd47d1936cacf
Zdenek Prusa <zdenek.prusa@gmail.com>
2016-07-15 11:07:47 +0200

    Updated version struct

--- src/version.c
+++ src/version.c
@@ -1,5 +1,10 @@
 #include "ltfat/version.h"
 
-static ltfat_library_version ltfat_version = { "0.1.0", "__DATE__ ## __TIME__" };
+static ltfat_library_version ltfat_version = { "0.1.0", __DATE__  " "  __TIME__,
+                                               0, 1 , 0
+                                             };
 
-ltfat_library_version* get_ltfat_library_version() { return &ltfat_version; }
+ltfat_library_version* get_ltfat_library_version()
+{
+    return &ltfat_version;
+}
