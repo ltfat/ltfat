d07f77445dc4316eefa09c3b37dbe700c471ca07
Zdenek Prusa <zdenek.prusa@gmail.com>
2017-07-18 21:55:25 +0200

    Moved fftw3.h the include dir

--- include/ltfat/complexcompat.h
+++ include/ltfat/complexcompat.h
@@ -20,34 +20,15 @@ using namespace std;
 #endif
 
 // Must be included after complex.h
-#include <fftw3.h>
+#include "ltfat/thirdparty/fftw3.h"
 #endif
 
 #if defined(__cplusplus) && !defined(LTFAT_CINTERFACE)
-    typedef std::complex<double> ltfat_complex_d;
-    typedef std::complex<float> ltfat_complex_s;
+typedef std::complex<double> ltfat_complex_d;
+typedef std::complex<float> ltfat_complex_s;
 #else
-#   if defined(_Complex_I) && defined(complex) && defined(I)
-        typedef complex double ltfat_complex_d;
-        typedef complex float ltfat_complex_s;
-#   else
-        typedef double ltfat_complex_d[2];
-        typedef float ltfat_complex_s[2] ;
-#   endif
+typedef fftw_complex ltfat_complex_d;
+typedef fftwf_complex ltfat_complex_s;
 #endif
-
-// #if defined(__cplusplus)
-// #   define ltfat_real(x) std::real(x)
-// #   define ltfat_imag(x) std::imag(x)
-// #   define ltfat_abs(x) std::abs(x)
-// #   define ltfat_arg(x) std::arg(x)
-// #else
-// #   define ltfat_complex_d(r,i) ((float)(r) + ((float)(i))*I)
-// #   define ltfat_complex_s(r,i) ((double)(r) + ((double)(i))*I)
-// #   define ltfat_real(x) creal(x)
-// #   define ltfat_imag(x) cimag(x)
-// #   define ltfat_abs(x) fabs(x)
-// #   define ltfat_arg(x) carg(x)
-// #endif
 
 #endif
