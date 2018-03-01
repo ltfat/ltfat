4e677268273d83c274b09739280af27ca781f966
Zdenek Prusa <zdenek.prusa@gmail.com>
2018-03-01 21:24:45 +0100

    Updated version.c
    The version check now works

--- src/version.c
+++ src/version.c
@@ -1,27 +1,30 @@
 #include "ltfat.h"
+#include "ltfat/macros.h"
 
 #define LTFAT_MAKEVESRIONSTRING(major,minor,patch) #major "." #minor "." #patch
 #define LTFAT_VERSIONSTRING(major,minor,patch) LTFAT_MAKEVESRIONSTRING(major,minor,patch)
 
 
-static ltfat_library_version ltfat_version =
+static const ltfat_library_version ltfat_version =
 {
     LTFAT_VERSIONSTRING(LTFAT_VERSION_MAJOR,LTFAT_VERSION_MINOR,LTFAT_VERSION_PATCH),
     __DATE__  " "  __TIME__,
-    LTFAT_VERSION_MAJOR, LTFAT_VERSION_MINOR , LTFAT_VERSION_PATCH
+    LTFAT_VERSION_MAJOR, LTFAT_VERSION_MINOR , LTFAT_VERSION_MICRO, sizeof(ltfat_int)
 };
 
-LTFAT_API ltfat_library_version*
+LTFAT_API const ltfat_library_version*
 ltfat_get_version()
 {
     return &ltfat_version;
 }
 
 LTFAT_API int
-ltfat_is_compatible_version()
+ltfat_is_compatible_version(unsigned int your_major,
+                            unsigned int UNUSED(your_minor),
+                            unsigned int UNUSED(your_micro))
 {
-    ltfat_library_version* dll_version = ltfat_get_version();
-    return (int) ( dll_version->major == LTFAT_VERSION_MAJOR);
+    const ltfat_library_version* dll_version = ltfat_get_version();
+    return (int) ( dll_version->major == your_major);
 }
 
 LTFAT_API int
