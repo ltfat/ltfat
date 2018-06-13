459ad7b522a7944266fa72b2121c0e48b3a9f1c0
Zdenek Prusa <zdenek.prusa@gmail.com>
2018-06-13 19:03:54 +0200

    Updated copyright in the license header

--- mat2doc/mat/header.m
+++ mat2doc/mat/header.m
@@ -1,4 +1,4 @@
-% Copyright (C) 2005-2016 Peter L. Soendergaard <peter@sonderport.dk>.
+% Copyright (C) 2005-2018 Peter L. Soendergaard <peter@sonderport.dk> and others.
 % This file is part of LTFAT version {VERSION}
 %
 % This program is free software: you can redistribute it and/or modify
