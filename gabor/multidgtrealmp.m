91b9f6c3be077418b488f4c4dc31437f1af3a924
Nicki Holighaus <nicki.holighaus@gmx.de>
2020-02-12 15:21:11 +0100

    Updated reset conditions in multidgtrealmp

--- gabor/multidgtrealmp.m
+++ gabor/multidgtrealmp.m
@@ -35,7 +35,9 @@ function [c, frec, info] = multidgtrealmp(f,dicts,varargin)
 %
 %     .atoms    Number of atoms selected.
 %
-%     .relres   Final approximation error. 
+%     .relres   Final approximation error. If resets are enabled, this is a
+%               vector additionally containing the approximation error at 
+%               every reset.
 %
 %     .g        Cell array of numeric windows used in the multi-dictionary
 %
@@ -50,25 +52,41 @@ function [c, frec, info] = multidgtrealmp(f,dicts,varargin)
 %                  `dim=1` if it was a row vector. 
 %
 %   The normalized approximation error is computed as 
-%   `err=norm(f-frec)/norm(f)`.
+%   `errdb=20*log10(norm(f-frec)/norm(f))`.
 %
 %   The function takes the following optional parameters at the end of
 %   the line of input arguments:
 %
-%     'kenrnthr',kt    Kernel threshold. Must be in range ]0,1]. Default is 1e-4.
+%     'kenrnthr',kt     Kernel threshold. Must be in range ]0,1]. 
+%                       Default is 1e-4.
 %
-%     'timeinv'        Use the time invariant phase convention. The
-%                      default is 'freqinv'.
+%     'timeinv'         Use the time invariant phase convention. The
+%                       default is 'freqinv'.
 %
-%     'pedanticsearch' Be pedantic about the energy of pairs of conjugated
-%                      atoms in the selection step. Disbaled by default.
+%     'pedanticsearch'  Be pedantic about the energy of pairs of 
+%                       conjugated atoms in the selection step. 
+%                       Disabled by default.
 %
-%     'algorithm',alg  Algorithm to use. Available: 
-%                      'mp'(default),'selfprojmp','cyclicmp'
+%     'algorithm',alg   Algorithm to use. Available: 
+%                       'mp'(default),'selfprojmp','cyclicmp'
 %
-%     'resetit',it     Reset the decomposition every `it` iteration.
-%                      The reset means a complete synthesis and re-analysis.
-%                      The default is 0 (no reset)
+%     'reset'           Reset the decomposition until stopping criteria
+%                       are met. The reset means a complete synthesis and 
+%                       re-analysis. Available: 
+%                       'noreset' (default), 'reset', 'quickreset'
+%                       Option 'quickreset' applies a heuristic to stop the 
+%                       algorithm more quickly when the targer error is 
+%                       reached.
+%
+%   Reset conditions can be controlled by the following key-value pairs:
+%
+%     'resetit',it      Reset the decomposition every `it` iteration.
+%                       Must be a positive integer.
+%
+%     'reseterrdb',err  Reset the decomposition when the error estimate 
+%                       in dB decreases below `err`. Overwrites 'resetit'.
+%                       Must be negative. 
+%                       Default is `10*log10(1.1*kv.kernthr)`.
 %
 %   The computational routine is only available in C. Use |ltfatmex| to
 %   to compile it.
@@ -84,6 +102,14 @@ function [c, frec, info] = multidgtrealmp(f,dicts,varargin)
 %   The kernel threshold limits the minimum approximation error which
 %   can be reached. For example the default threshold 1e-4 in general 
 %   allows achieving at least -40 dB.
+% 
+%   Note that this algorithm internally computes the error as
+%   `errdb = 10*log10(err(k)/norm(f)^2)`, with `err(0) = norm(f)^2` and 
+%   `err(k) = err(k-1) - abs(cselected(k))^2`. Here, `cselected(k)` is the
+%   Gabor coefficient selected at the k-th MP step. This formula is exact 
+%   only if the kernel threshold equals 0. Otherwise it serves as a cheap
+%   estimate. The exact error is only computed when this function is run
+%   with resets enabled and only in between resets. 
 %   
 %   Examples
 %   --------
@@ -114,6 +140,7 @@ complainif_notenoughargs(nargin,2,thismfile);
 definput.keyvals.errdb=-40;
 definput.keyvals.maxit=[];
 definput.keyvals.resetit=0;
+definput.keyvals.reseterrdb = 0;
 %definput.keyvals.iterstep=[];
 definput.keyvals.kernthr = 1e-4;
 %definput.flags.print={'quiet','print'};
@@ -121,6 +148,8 @@ definput.flags.algversion={'fast','slow'};
 definput.flags.algorithm={'mp','selfprojmp','cyclicmp'};
 definput.flags.search={'plainsearch','pedanticsearch'};
 definput.flags.phaseconv={'freqinv','timeinv'};
+definput.flags.reset={'noreset','reset','quickreset'};
+
 [flags,kv]=ltfatarghelper({'errdb','maxit'},definput,varargin);
 
 if exist('comp_multidgtrealmp','file') ~= 3 && flags.do_fast
@@ -130,6 +159,25 @@ end
 
 if flags.do_slow
     error('%s: ''slow'' is not supported yet.',thismfile)
+end
+
+if flags.do_reset || flags.do_quickreset % Check reseterrdb or resetit
+   if kv.reseterrdb == 0
+        if kv.resetit == 0 % Set both to conservative default values depending on kernthr
+            kv.reseterrdb = 10*log10(1.1*kv.kernthr);
+            kv.resetit = round(1000/sqrt(kv.kernthr)); 
+        else % If only resetit is given, ignore reseterrdb
+            kv.reseterrdb = -flintmax;
+        end
+   elseif kv.resetit == 0 % If only reseterrdb is given, ignore resetit
+        kv.resetit = flintmax;
+   end   
+   if kv.reseterrdb >= 0
+       error('%s: Reset error tolerance must be negative.',thismfile);
+   end
+   if kv.resetit < 0 || kv.resetit ~= round(kv.resetit)
+       error('%s: Number of iterations before reset must be a positive integer.',thismfile);
+   end
 end
 
 %% ----- step 1 : Verify f and determine its length -------
@@ -145,7 +193,7 @@ if kv.errdb > 0
 end
 
 if ~(kv.kernthr > 0 && kv.kernthr <= 1)
-    error('%s: Kenel threshold must be in range ]0,1].',upper(mfilename));
+    error('%s: Kernel threshold must be in range ]0,1].',upper(mfilename));
 end
 
 if ~iscell(dicts), error('%s: dicts must be cell',thismfile); end
@@ -200,7 +248,8 @@ end
 % Initial residuum
 fpad = postpad(f,L);
 
-if kv.resetit == 0
+% Determine reset/no reset mode and run MP decomposition 
+if flags.do_noreset
     [c,info.atoms,info.iter,info.status] = ...
         comp_multidgtrealmp(fpad,info.g,a,M,flags.do_timeinv,...
                             kv.kernthr,kv.errdb,kv.maxit,kv.maxit,...
@@ -213,13 +262,22 @@ else
 
     remainingit = kv.maxit;
     currerrdb = kv.errdb;
+        
+    info.resets = -1;
+    reseterrdb = kv.reseterrdb;
 
-    while remainingit > 0 
+    while remainingit > 0 && currerrdb < 0
+        info.resets = info.resets + 1;
+        
+        % Adjust number of iterations
         currit = min([remainingit,kv.resetit]);
+        if flags.do_quickreset % Adjust reset error condition
+            reseterrdb = max([1.2*currerrdb,kv.reseterrdb]);
+        end
 
         [ctmp,info.atoms,info.iter,info.status] = ...
             comp_multidgtrealmp(fpad,info.g,a,M,flags.do_timeinv,...
-                                kv.kernthr,currerrdb,currit,currit,...
+                                kv.kernthr,reseterrdb,currit,currit,...
                                 flags.do_pedanticsearch, flags.algorithm );
 
         c = cellfun(@(cEl,ctmpEl) cEl + ctmpEl, c(:)',ctmp(:)','UniformOutput', 0);
@@ -227,7 +285,7 @@ else
         remainingit = remainingit - info.iter;
 
         %The following means that some other stopping criterion has been reached
-        if currit ~= info.iter, break; end
+        if info.status ~=0 && info.status ~=3 && currit ~= info.iter, break; end
 
         if remainingit > 0
             % Recompute residuum
@@ -235,8 +293,16 @@ else
                    c(:)',info.g(:)',num2cell(a(:))',num2cell(M(:))','UniformOutput',0)),2);
             
             fpad = postpad(f,L) - frec;
+            relres = norm(fpad(:)) / fnorm;            
             % Since we are changing the residual, we must adjust the target error
-            currerrdb = kv.errdb - 20*log10( norm(fpad(:)) / fnorm );
+            currerrdb = kv.errdb - 20*log10(relres);            
+            if currerrdb < 0
+                info.relres(info.resets+1) = relres;
+            end
+            if info.resets >= 1 && info.relres(end) > info.relres(end-1)
+                info.status = 6; 
+                break; 
+            end
         end
     end
 
@@ -257,18 +323,26 @@ if nargout>1
   if dim == 2, dim2 = 1; end
   frec = sum(info.synthetize(c),dim2);
   if fnorm == 0
-    info.relres = 0;
+    info.relres(end+1) = 0;
   else
-    info.relres = norm(frec(:)-f(:))/fnorm;
+    info.relres(end+1) = norm(frec(:)-f(:))/fnorm;
   end
-
+  
+  % Fix the status
+  if info.relres(end) <= 10^(kv.errdb/20)
+      info.status = 0;
+  elseif info.iter == kv.maxit
+      info.status = 2;
+  end
+  
   status_str = {...
   'Target error reached',...
   'Maximum number of atoms reached',...
   'Maximum number of iterations reached',...
   'Stalled (abs. norm. error estimate became negative)',...
   'Selected coefficient tolerance reached',...
-  'All zeros'...
+  'All zeros',...
+  'Stalled (Residual has increased since last reset. Try to reduce resetit.)'...
   };
   info.message = status_str{1 + info.status};
 end
