e4efcc4849a88d1cb2b9e2fdf3bfe40745c2953e
allthatsounds <chollomey@kfs.oeaw.ac.at>
2021-06-25 15:58:59 +0200

    fixes #77

--- sigproc/firkaiser.m
+++ sigproc/firkaiser.m
@@ -20,11 +20,16 @@ function g=firkaiser(L,beta,varargin)
 %     'hp'       Generate half point even window.
 %  
 %   Additionally, `firkaiser` accepts flags to normalize the output. Please
-%   see the help of |normalize|. Default is to use `'peak'` normalization.
+%   see the help of |normalize|. Default is to use `'null'` normalization.
+%
+%   Note that odd-length Derived Kaiser-Bessel windows are not
+%   mathematically defined, yet they are supported by this code.
 %
 %   See also: firwin, normalize
 %
 %   References: opsc89
+
+%   AUTHOR: unknown. Additions by Clara Hollomey
 
 if nargin<2
   error('Too few input arguments.');
@@ -48,48 +53,48 @@ if flags.do_hp
 end;
 
 if flags.do_normal
-  
+ 
   if (L == 1)
     g = 1;
   else
     m = L - 1;
-    k = (0:L-1)'+rem(L,2)/2-.5+cent;
-    k = 2*beta/(L-1)*sqrt(k.*(L-1-k));
+    k = (0:m)'+rem(L,2)/2-.5+cent;
+    k = 2*beta/(m)*sqrt(k.*(m-k));
     g = besseli(0,k)/besseli(0,beta);
   end;
 
   g=ifftshift(g);
-  
+ 
   if ((flags.do_wp && rem(L,2)==0) || ...
       (flags.do_hp && rem(L,2)==1))
-    
+   
     % Explicitly zero last element. This is done to get the right
-    % symmetry, and because that element sometimes turn negative.
+    % symmetry, and because that element sometimes turns negative.
     g(floor(L/2)+1)=0;
   end;
-  
+ 
 else
-  
-  if rem(L,2)==1    
-    error('The length of the choosen window must be even.');
-  end;
-  
-  if flags.do_wp 
-    if rem(L,4)==0
-      L2=L/2+2;
-    else
-      L2=L/2+1;
-    end;
+ 
+  %if rem(L,2)==1    
+  %  error('The length of the choosen window must be even.');
+  %end;
+ 
+  if flags.do_wp
+    %if rem(L,4)==0
+    %  L2=L/2+2;
+    %else
+      L2=floor(L/2+1);
+    %end;
   else
     L2=floor((L+1)/2);
   end;
-  
+ 
   % Compute a normal Kaiser window
   g_normal=fftshift(firkaiser(L2,beta,flags.centering));
-  
+ 
   g1=sqrt(cumsum(g_normal(1:L2))./sum(g_normal(1:L2)));
-  
-  if flags.do_wp 
+ 
+  if flags.do_wp
     if rem(L,2)==0
       g=[flipud(g1);...
          g1(2:L/2)];
@@ -98,23 +103,26 @@ else
          g1(1:floor(L/2))];
     end;    
   else
-    g=[flipud(g1);...
-       g1];
+      if rem(L,2)==0
+         g=[flipud(g1);0;...
+         g1(1:end-1)];
+      else
+         g=[flipud(g1);...
+         g1(1:end-1)];
+      end
   end;
 
-    if ((flags.do_wp && rem(L,2)==0) || ...
-      (flags.do_hp && rem(L,2)==1))
-    
+  if ((flags.do_wp && rem(L,2)==0)) %|| ...
+      %(flags.do_hp && rem(L,2)==1))
+   
     % Explicitly zero last element. This is done to get the right
     % symmetry, and because that element sometimes turn negative.
     g(floor(L/2)+1)=0;
   end;
-
-  
+ 
 end;
 
 % The besseli computation sometimes generates a zero imaginary component.
 g=real(g);
 
 g=normalize(g,flags.norm);
-
