75a467f5b72dad1b02a78f257119a1cb2d79cc1e
allthatsounds <clara.hollomey@allthatsounds.com>
2022-09-25 22:46:32 +0200

    complemented demo_waveletfilters

--- demos/demo_waveletfilters.m
+++ demos/demo_waveletfilters.m
@@ -1,6 +1,10 @@
 %DEMO_WAVELETFILTERS  Introduction to grid-like wavelet sampling
 %
-%   This demo shows how to perform grid-like wavelet sampling.
+%   This demo showcases three options to generate a wavelet filter bank.
+%
+%   .. figure::
+%
+%      Invertible wavelet filterbank with linearly spaced center frequencies.
 %
 %   .. figure::
 %
@@ -9,70 +13,72 @@
 %      The figure shows the filter bank response with linear scales.
 %
 %
-%   See also: waveletfilters, freqwavelet, filterbank, filterbankrealdual
+%   See also: waveletfilters, freqwavelet, lowdiscrepancy, filterbankrealdual
 
 %the input signal
 [f,fs]=gspi;
 Ls = length(f);
 
 %wavelet types
-wvlt = {{'cauchy', 300}, {'morlet', 3}};
+wvlt = {{'cauchy', 600}, {'morlet', 3}};
 
-%the frequency range and spacing to be covered
-M0 = 511; %desired number of channels (without low frequency compensation channels)
-fmax = fs/2; %Nyquist frequency
-fmin = 20;
+cauchyalpha = [300, 700, 1200, 1700, 2100];
+
+%design an invertible wavelet filter bank with linearly spaced center
+%frequencies
+fs = 2;
+M = 64;
+MC = 3;
+fmin = MC/M;
+fmax = fs/2; %up to Nyquist frequency
+numscales = M-MC+1;
+redundancy = 4;
+delays = lowdiscrepancy('digital');
+
+%calling waveletfilters by passing the frequency range
+[g,a,fc, L]=waveletfilters(Ls,'linear',fs,fmin,fmax,numscales,...
+    {'cauchy',cauchyalpha(1)},'uniform','redtar',redundancy,'repeat','delay',delays);
+
+c = filterbank(f, g, a);
+    
+figure(1)
+plotfilterbank(c, a)
+
+
+%call waveletfilters by passing the scales directly
+M = 127; %desired number of channels (without low frequency compensation channels)
 
 %derive the associated scales
 max_freq = 10;  % 10 corresponds to waveltfilters' internal Nyquist frequency
-freq_step = max_freq/M0;
-min_freqHz = fs/10*freq_step;
-start_index = 6; %set the desired number of compensation filters
-min_scale_freq = min_freqHz*start_index;
-min_freqDiv10 = freq_step*start_index;
-scales = 1./linspace(min_freqDiv10,max_freq,M0);
+min_freq = max_freq/M;
+scales = 1./linspace(min_freq,max_freq,M);
+%calling waveletfilters via scales
+[g, a,fc_scales,L,info] = waveletfilters(Ls,scales,wvlt{2},'uniform',...
+    'single','energy', 'delay',delays, 'redtar', 8);
 
-%define the delay function
-alpha = 1-2/(1+sqrt(5)); % 1-1/(goldenratio) delay sequence
-delays = @(n,a) a*(mod(n*alpha+.5,1)-.5);
+gd=filterbankrealdual(g,a,L);
+% Plot frequency responses of individual filters
+figure(2);
+subplot(2,1,1);
+filterbankfreqz(gd,a,L,fs,'plot','linabs','posfreq');
 
-%calling waveletfilters by passing the frequency range
-[~, ~,fc_freqlin,~,~] = waveletfilters(Ls,'linear', fs, fmin, fmax, M0);
+subplot(2,1,2);
+filterbankfreqz(g,a,L,fs,'plot','linabs','posfreq');
+    
 
 %calling waveletfilters in a constant-Q style
-[~, ~,fc_cqt,~,~] = waveletfilters(Ls,'bins', fs, fmin, fmax, 50);
+fmin = 20;
+fmax = 8000;
+bins = 16;
+fs = 16000;
+[g, a,fc_cqt, L] = waveletfilters(Ls,'bins', fs, fmin, fmax, bins, 'delay', lowdiscrepancy('kronecker'));
 
-%calling waveletfilters via scales
-[g, a,fc_scales,L,info] = waveletfilters(Ls,scales,wvlt{1},'uniform',...
-    'repeat','energy', 'delay',delays, 'redtar', 4);
 
-fbbounds = filterbankrealbounds(g,a,L);
-
-if ~isinf(fbbounds)
-    gd=filterbankrealdual(g,a,L);
-    % Plot frequency responses of individual filters
-    figure(1);
-    subplot(2,1,1);
-    filterbankfreqz(gd,a,L,fs,'plot','linabs','posfreq');
-
-    subplot(2,1,2);
-    filterbankfreqz(g,a,L,fs,'plot','linabs','posfreq');
-    
-    c=filterbank(f,gd,a);
-    fcons=2*real(ifilterbank(c,g,a));
-    if length(fcons) > length(f)
-        err=norm(fcons(1:length(f))-f)
-    else
-        err=norm(fcons-f(1:length(fcons)))
-    end
-    fprintf('Reconstruction error:      %e\n',err);
+c=filterbank(f,g,a);
+[fpcg,~,iterpcg] = ifilterbankiter(c,g,a,'pcg');
+if length(fpcg) > length(f)
+    err=norm(fpcg(1:length(f))-f);
 else
-    c=filterbank(f,g,a);
-    [fpcg,~,iterpcg] = ifilterbankiter(c,g,a,'pcg');
-    if length(fpcg) > length(f)
-        err=norm(fpcg(1:length(f))-f);
-    else
-        err=norm(fpcg-f(1:length(fpcg)));
-    end
-    fprintf('Reconstruction error:      %e\n',err);
+    err=norm(fpcg-f(1:length(fpcg)));
 end
+fprintf('Reconstruction error:      %e\n',err);
