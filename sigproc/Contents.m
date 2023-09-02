d27348d81040a37bf42c73a9b073fbef4d5603c3
allthatsounds <clara.hollomey@allthatsounds.com>
2023-09-03 00:55:48 +0200

    updated Contents files for upcoming release

--- sigproc/Contents.m
+++ sigproc/Contents.m
@@ -1,6 +1,6 @@
 % LTFAT - Signal processing tools
 %
-%  Peter L. Søndergaard, 2007 - 2018.
+%  Peter L. Søndergaard, 2007 - 2023.
 %
 %  General
 %    RMS            -  Root Mean Square norm of signal.
