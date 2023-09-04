547acbbb964c40f805818bf5d2e5eb66cef0413e
allthatsounds <clara.hollomey@allthatsounds.com>
2023-09-05 00:05:58 +0200

    added routine for instantiating fusion frames

---
+++ comp/arg_fusionframe.m
@@ -0,0 +1,7 @@
+function definput=arg_fusionframe(definput)
+
+definput.keyvals.dgt = {'gauss', 10, 20};
+definput.keyvals.dwilt = {'gauss',20};
+definput.keyvals.wmdct = {'gauss',20};
+definput.keyvals.fwt = {'db8',8};
+definput.keyvals.dsti = eye(10);
