6b94643b34687eca06bd2709464e54f8e80feb27
allthatsounds <clara.hollomey@allthatsounds.com>
2023-10-09 17:03:39 +0200

    added ltfatinfo prototype

---
+++ ltfatinfo.m
@@ -0,0 +1,9 @@
+function info = ltfatinfo(functionname)
+%LTFATINFO  Information on the input parameters and dependencies of an LTFAT function
+%   Usage: info = ltfatinfo(functionname);
+%
+%
+%   `ltfatinfo(functionname)` returns a struct with the input and output
+%   arguments of a function.
+
+
